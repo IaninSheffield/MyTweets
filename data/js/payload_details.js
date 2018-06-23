2872da782571672bf3e355814d7ee5c93dac1969
IaninSheffield <newreg@guesty.me.uk>
2018-06-23 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10864,
-	"created_at": "Fri Jun 22 01:15:58 +0000 2018",
+	"tweets": 10865,
+	"created_at": "Sat Jun 23 01:15:57 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,265 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Fri Jun 22 19:21:37 +0000 2018",
+		"id": 1010241416063316000,
+		"id_str": "1010241416063315969",
+		"text": "Fascinating studies brimming with ethical dilemmas to discuss from participants at today's @BERA_ECRNetwork symposi… https://t.co/kYVwjhY8Ic",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BERA_ECRNetwork",
+					"name": "BERA ECR Network",
+					"id": 878352014010458100,
+					"id_str": "878352014010458113",
+					"indices": [
+						91,
+						107
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kYVwjhY8Ic",
+					"expanded_url": "https://twitter.com/i/web/status/1010241416063315969",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. Now doctoral researcher @sheffhallamuni exploring teacher professional learning.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3207,
+			"friends_count": 3752,
+			"listed_count": 208,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 459,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10859,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1010207866970628100,
+		"quoted_status_id_str": "1010207866970628097",
+		"quoted_status": {
+			"created_at": "Fri Jun 22 17:08:18 +0000 2018",
+			"id": 1010207866970628100,
+			"id_str": "1010207866970628097",
+			"text": "A fantastic symposium with lots of exciting ethical discussions @StaffsUni today! Thanks to all our presenters, our… https://t.co/G0xqG5ByM5",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "StaffsUni",
+						"name": "Staffordshire Uni",
+						"id": 66952977,
+						"id_str": "66952977",
+						"indices": [
+							64,
+							74
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/G0xqG5ByM5",
+						"expanded_url": "https://twitter.com/i/web/status/1010207866970628097",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 878352014010458100,
+				"id_str": "878352014010458113",
+				"name": "BERA ECR Network",
+				"screen_name": "BERA_ECRNetwork",
+				"location": "United Kingdom",
+				"description": "Convenors: @OliverRHooper and @YuweiXu // NB: All views expressed are those of the Network convenors, not those of BERA",
+				"url": "https://t.co/yeB8l7hdZx",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/yeB8l7hdZx",
+								"expanded_url": "http://www.bera.ac.uk/group/ecrnetwork",
+								"display_url": "bera.ac.uk/group/ecrnetwo…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 278,
+				"friends_count": 116,
+				"listed_count": 1,
+				"created_at": "Fri Jun 23 20:40:13 +0000 2017",
+				"favourites_count": 94,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 122,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/986992422671585281/_aoWQtNH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/986992422671585281/_aoWQtNH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/878352014010458113/1524152356",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "50f2d0272381533f",
+				"url": "https://api.twitter.com/1.1/geo/id/50f2d0272381533f.json",
+				"place_type": "admin",
+				"name": "East Midlands",
+				"full_name": "East Midlands, England",
+				"country_code": "GB",
+				"country": "United Kingdom",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-2.03410488116464,
+								51.9772264213839
+							],
+							[
+								0.357994000542651,
+								51.9772264213839
+							],
+							[
+								0.357994000542651,
+								53.6163564980205
+							],
+							[
+								-2.03410488116464,
+								53.6163564980205
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jun 21 17:37:59 +0000 2018",
 		"id": 1009852948552343600,
 		"id_str": "1009852948552343552",
