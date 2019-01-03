04983252ab7462eb7ce3344c3f56e536587c5264
IaninSheffield <newreg@guesty.me.uk>
2019-01-03 01:15:59 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11314,
-	"created_at": "Wed Jan 02 01:15:58 +0000 2019",
+	"tweets": 11315,
+	"created_at": "Thu Jan 03 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -49,6 +49,101 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 234264,
+			"friends_count": 1370,
+			"listed_count": 9171,
+			"created_at": "Thu Jul 13 21:27:23 +0000 2006",
+			"favourites_count": 98,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5652,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1005181261017567232/tRU1yGhT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1005181261017567232/tRU1yGhT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/633/1404311276",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 339,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Academic friends: what are your all-time favorite ethnographies? I’m looking for brain candy."
+	},
+	{
+		"created_at": "Tue Jan 01 17:02:18 +0000 2019",
+		"id": 1080147213781745700,
+		"id_str": "1080147213781745664",
+		"full_text": "Academic friends: what are your all-time favorite ethnographies? I’m looking for brain candy.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 633,
+			"id_str": "633",
+			"name": "danah boyd",
+			"screen_name": "zephoria",
+			"location": "New York, NY",
+			"description": "sociotechnical researcher | Microsoft Research, Data & Society (@datasociety), NYU | zephoria@zephoria.org",
+			"url": "https://t.co/4ST5UQLw6b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4ST5UQLw6b",
+							"expanded_url": "http://www.danah.org/",
+							"display_url": "danah.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 234316,
 			"friends_count": 1370,
 			"listed_count": 9171,
