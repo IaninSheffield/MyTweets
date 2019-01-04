3aa83f42ee6022fe3a605e6e1dfa652ff8629f0e
IaninSheffield <newreg@guesty.me.uk>
2019-01-04 01:15:58 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11315,
-	"created_at": "Thu Jan 03 01:15:57 +0000 2019",
+	"tweets": 11317,
+	"created_at": "Fri Jan 04 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,393 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 02 11:25:30 +0000 2019",
+		"id": 1080424842921414700,
+		"id_str": "1080424842921414656",
+		"full_text": "Just met an amazing man in Endcliffe Park, Sheffield. Tony Foulds was an 8-yr-old playing in the park when a US plane crashed in Feb 1944. He has diligently maintained the memorial ever since. He was planting new flowers. Almost 75 yrs of service. What a man. I’m in bits 😢👏🏻 https://t.co/DpdDmSZ0F3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1080424826093924400,
+					"id_str": "1080424826093924352",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080424826093924400,
+					"id_str": "1080424826093924352",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825645137900,
+					"id_str": "1080424825645137920",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5MXgAAbZyA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5MXgAAbZyA.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825653481500,
+					"id_str": "1080424825653481472",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5OW0AAxDz0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5OW0AAxDz0.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825645088800,
+					"id_str": "1080424825645088770",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5MWwAIB7xL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5MWwAIB7xL.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 47945492,
+			"id_str": "47945492",
+			"name": "Dan Walker",
+			"screen_name": "mrdanwalker",
+			"location": "Usually in the North",
+			"description": "Dad, husband, that @BBCbreakfast & @BBCSport bloke. Agent: Jonny.mcwilliams@crowntalentgroup.com Insta: https://t.co/dtY04Gw3Jl",
+			"url": "https://t.co/forkvWOApW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/forkvWOApW",
+							"expanded_url": "http://www.danwalker.tv",
+							"display_url": "danwalker.tv",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/dtY04Gw3Jl",
+							"expanded_url": "https://www.instagram.com/mrdanwalker",
+							"display_url": "instagram.com/mrdanwalker",
+							"indices": [
+								104,
+								127
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 526484,
+			"friends_count": 1206,
+			"listed_count": 2725,
+			"created_at": "Wed Jun 17 12:35:35 +0000 2009",
+			"favourites_count": 3399,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 53529,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1042676979634515968/RHQySk_K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1042676979634515968/RHQySk_K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/47945492/1540156284",
+			"profile_link_color": "2200FF",
+			"profile_sidebar_border_color": "1B0D4D",
+			"profile_sidebar_fill_color": "BC95DE",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2426,
+		"favorite_count": 14662,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just met an amazing man in Endcliffe Park, Sheffield. Tony Foulds was an 8-yr-old playing in the park when a US plane crashed in Feb 1944. He has diligently maintained the memorial ever since. He was planting new flowers. Almost 75 yrs of service. What a man. I’m in bits 😢👏🏻 https://t.co/DpdDmSZ0F3"
+	},
+	{
+		"created_at": "Tue Jan 01 17:02:18 +0000 2019",
+		"id": 1080147213781745700,
+		"id_str": "1080147213781745664",
+		"full_text": "Academic friends: what are your all-time favorite ethnographies? I’m looking for brain candy.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 633,
+			"id_str": "633",
+			"name": "danah boyd",
+			"screen_name": "zephoria",
+			"location": "New York, NY",
+			"description": "sociotechnical researcher | Microsoft Research, Data & Society (@datasociety), NYU | zephoria@zephoria.org",
+			"url": "https://t.co/4ST5UQLw6b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4ST5UQLw6b",
+							"expanded_url": "http://www.danah.org/",
+							"display_url": "danah.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 234234,
+			"friends_count": 1370,
+			"listed_count": 9173,
+			"created_at": "Thu Jul 13 21:27:23 +0000 2006",
+			"favourites_count": 98,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5652,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1005181261017567232/tRU1yGhT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1005181261017567232/tRU1yGhT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/633/1404311276",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 68,
+		"favorite_count": 396,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Academic friends: what are your all-time favorite ethnographies? I’m looking for brain candy."
+	},
+	{
 		"created_at": "Tue Jan 01 17:02:18 +0000 2019",
 		"id": 1080147213781745700,
 		"id_str": "1080147213781745664",
