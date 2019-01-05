0cb4268f1fe6efe26e7f3960f7b40a694ef2a5a0
IaninSheffield <newreg@guesty.me.uk>
2019-01-05 01:15:58 +0000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11317,
-	"created_at": "Fri Jan 04 01:15:57 +0000 2019",
+	"tweets": 11320,
+	"created_at": "Sat Jan 05 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,608 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 04 21:04:12 +0000 2019",
+		"id": 1081295255209762800,
+		"id_str": "1081295255209762819",
+		"full_text": "@hattisambrooks Indeed.\nRecommendation from the RCPCH \"The health impacts of screen time - a guide for clinicians and parents\" https://t.co/KU77s5UZPi https://t.co/4oSpmPwFjS",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hattisambrooks",
+					"name": "Hatti Sambrooks",
+					"id": 826023083895038000,
+					"id_str": "826023083895037952",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/KU77s5UZPi",
+					"expanded_url": "https://www.rcpch.ac.uk/resources/health-impacts-screen-time-guide-clinicians-parents",
+					"display_url": "rcpch.ac.uk/resources/heal…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1081294588093087700,
+					"id_str": "1081294588093087745",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/DwGHsuzXQAEGdrf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwGHsuzXQAEGdrf.jpg",
+					"url": "https://t.co/4oSpmPwFjS",
+					"display_url": "pic.twitter.com/4oSpmPwFjS",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1081295255209762819/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 108,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 803,
+							"h": 128,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 128,
+							"h": 128,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 803,
+							"h": 128,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1081294588093087700,
+					"id_str": "1081294588093087745",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/DwGHsuzXQAEGdrf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwGHsuzXQAEGdrf.jpg",
+					"url": "https://t.co/4oSpmPwFjS",
+					"display_url": "pic.twitter.com/4oSpmPwFjS",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1081295255209762819/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 108,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 803,
+							"h": 128,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 128,
+							"h": 128,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 803,
+							"h": 128,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1081284019877220400,
+		"in_reply_to_status_id_str": "1081284019877220354",
+		"in_reply_to_user_id": 826023083895038000,
+		"in_reply_to_user_id_str": "826023083895037952",
+		"in_reply_to_screen_name": "hattisambrooks",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3205,
+			"friends_count": 3787,
+			"listed_count": 205,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 588,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11254,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@hattisambrooks Indeed.\nRecommendation from the RCPCH \"The health impacts of screen time - a guide for clinicians and parents\" https://t.co/KU77s5UZPi https://t.co/4oSpmPwFjS"
+	},
+	{
+		"created_at": "Fri Jan 04 07:31:19 +0000 2019",
+		"id": 1081090684910743600,
+		"id_str": "1081090684910743552",
+		"full_text": "BBC - \"Worry less about children's screen use, parents told\" https://t.co/SHCP8RAhuK\nOr\nThe Times - \"Doctors tell parents to cut children’s screen time\" https://t.co/uivK3eYA9Z\nHeadlines based on the same source.\nGood luck to parents sorting that one out!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SHCP8RAhuK",
+					"expanded_url": "https://www.bbc.co.uk/news/health-46749232",
+					"display_url": "bbc.co.uk/news/health-46…",
+					"indices": [
+						61,
+						84
+					]
+				},
+				{
+					"url": "https://t.co/uivK3eYA9Z",
+					"expanded_url": "https://www.thetimes.co.uk/article/doctors-tell-parents-to-cut-children-s-screen-time-p3hp5hf8x",
+					"display_url": "thetimes.co.uk/article/doctor…",
+					"indices": [
+						153,
+						176
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.echofon.com\" rel=\"nofollow\">Echofon  Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3205,
+			"friends_count": 3787,
+			"listed_count": 205,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 588,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11254,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BBC - \"Worry less about children's screen use, parents told\" https://t.co/SHCP8RAhuK\nOr\nThe Times - \"Doctors tell parents to cut children’s screen time\" https://t.co/uivK3eYA9Z\nHeadlines based on the same source.\nGood luck to parents sorting that one out!"
+	},
+	{
+		"created_at": "Wed Jan 02 11:25:30 +0000 2019",
+		"id": 1080424842921414700,
+		"id_str": "1080424842921414656",
+		"full_text": "Just met an amazing man in Endcliffe Park, Sheffield. Tony Foulds was an 8-yr-old playing in the park when a US plane crashed in Feb 1944. He has diligently maintained the memorial ever since. He was planting new flowers. Almost 75 yrs of service. What a man. I’m in bits 😢👏🏻 https://t.co/DpdDmSZ0F3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1080424826093924400,
+					"id_str": "1080424826093924352",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080424826093924400,
+					"id_str": "1080424826093924352",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp63XcAA81DJ.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825645137900,
+					"id_str": "1080424825645137920",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5MXgAAbZyA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5MXgAAbZyA.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825653481500,
+					"id_str": "1080424825653481472",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5OW0AAxDz0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5OW0AAxDz0.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1080424825645088800,
+					"id_str": "1080424825645088770",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wp5MWwAIB7xL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wp5MWwAIB7xL.jpg",
+					"url": "https://t.co/DpdDmSZ0F3",
+					"display_url": "pic.twitter.com/DpdDmSZ0F3",
+					"expanded_url": "https://twitter.com/mrdanwalker/status/1080424842921414656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 47945492,
+			"id_str": "47945492",
+			"name": "Dan Walker",
+			"screen_name": "mrdanwalker",
+			"location": "Usually in the North",
+			"description": "Dad, husband, that @BBCbreakfast & @BBCSport bloke. Agent: Jonny.mcwilliams@crowntalentgroup.com Insta: https://t.co/dtY04Gw3Jl",
+			"url": "https://t.co/forkvWOApW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/forkvWOApW",
+							"expanded_url": "http://www.danwalker.tv",
+							"display_url": "danwalker.tv",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/dtY04Gw3Jl",
+							"expanded_url": "https://www.instagram.com/mrdanwalker",
+							"display_url": "instagram.com/mrdanwalker",
+							"indices": [
+								104,
+								127
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 527335,
+			"friends_count": 1211,
+			"listed_count": 2726,
+			"created_at": "Wed Jun 17 12:35:35 +0000 2009",
+			"favourites_count": 3414,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 53588,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1042676979634515968/RHQySk_K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1042676979634515968/RHQySk_K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/47945492/1540156284",
+			"profile_link_color": "2200FF",
+			"profile_sidebar_border_color": "1B0D4D",
+			"profile_sidebar_fill_color": "BC95DE",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3562,
+		"favorite_count": 18804,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just met an amazing man in Endcliffe Park, Sheffield. Tony Foulds was an 8-yr-old playing in the park when a US plane crashed in Feb 1944. He has diligently maintained the memorial ever since. He was planting new flowers. Almost 75 yrs of service. What a man. I’m in bits 😢👏🏻 https://t.co/DpdDmSZ0F3"
+	},
+	{
 		"created_at": "Wed Jan 02 11:25:30 +0000 2019",
 		"id": 1080424842921414700,
 		"id_str": "1080424842921414656",
