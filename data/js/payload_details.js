75d9d3e36e789db4a0b5b859925469b601369f46
IaninSheffield <newreg@guesty.me.uk>
2019-03-18 01:15:58 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11404,
-	"created_at": "Sun Mar 17 01:15:58 +0000 2019",
+	"tweets": 11406,
+	"created_at": "Mon Mar 18 01:15:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,327 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 17 19:18:32 +0000 2019",
+		"id": 1107360586906718200,
+		"id_str": "1107360586906718208",
+		"full_text": "Hardly a figure to be proud of, but one thing recent times have encouraged me to do is to dig a little deeper.\nAnother way to spin this is that the nat. debt has only(?!) doubled https://t.co/xUDhif8cdU\nMany other ways to frame these figures, depending on your political leaning. https://t.co/c6M34qGMDa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xUDhif8cdU",
+					"expanded_url": "https://www.ukpublicspending.co.uk/uk_national_debt_analysis",
+					"display_url": "ukpublicspending.co.uk/uk_national_de…",
+					"indices": [
+						179,
+						202
+					]
+				},
+				{
+					"url": "https://t.co/c6M34qGMDa",
+					"expanded_url": "https://twitter.com/suey2y/status/1107236331623727104",
+					"display_url": "twitter.com/suey2y/status/…",
+					"indices": [
+						280,
+						303
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3229,
+			"friends_count": 3803,
+			"listed_count": 205,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 605,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11344,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1107236331623727100,
+		"quoted_status_id_str": "1107236331623727104",
+		"quoted_status_permalink": {
+			"url": "https://t.co/c6M34qGMDa",
+			"expanded": "https://twitter.com/suey2y/status/1107236331623727104",
+			"display": "twitter.com/suey2y/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Mar 17 11:04:47 +0000 2019",
+			"id": 1107236331623727100,
+			"id_str": "1107236331623727104",
+			"full_text": "OMG!!!! National debt went over TWO TRILLION and I didn't even see it reported. It was £700 Billion when Labour left power!! They've TRIPLED NATIONAL DEBT!!!! TRIPLED (Debt increasing at £18 million an HOUR)",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				207
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 32536660,
+				"id_str": "32536660",
+				"name": "Sue Marsh",
+				"screen_name": "suey2y",
+				"location": "Sussex",
+				"description": "Rebel without a bowel. Political soothsayer. Black-belt in charity shopping. Disability and health warrior. Carpe Diem. Quam minimum credula postero",
+				"url": "https://t.co/VPbPVxv9lc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VPbPVxv9lc",
+								"expanded_url": "http://diaryofabenefitscrounger.blogspot.com",
+								"display_url": "diaryofabenefitscrounger.blogspot.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11472,
+				"friends_count": 5832,
+				"listed_count": 409,
+				"created_at": "Fri Apr 17 19:36:34 +0000 2009",
+				"favourites_count": 33503,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 146123,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FAF5F6",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/824526890232926208/3AN2NJxO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/824526890232926208/3AN2NJxO_normal.jpg",
+				"profile_link_color": "940840",
+				"profile_sidebar_border_color": "D95B43",
+				"profile_sidebar_fill_color": "F0F5F5",
+				"profile_text_color": "0F0F0E",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1743,
+			"favorite_count": 1809,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hardly a figure to be proud of, but one thing recent times have encouraged me to do is to dig a little deeper.\nAnother way to spin this is that the nat. debt has only(?!) doubled https://t.co/xUDhif8cdU\nMany other ways to frame these figures, depending on your political leaning. https://t.co/c6M34qGMDa"
+	},
+	{
+		"created_at": "Sun Mar 17 18:51:49 +0000 2019",
+		"id": 1107353863382880300,
+		"id_str": "1107353863382880257",
+		"full_text": "@hopkinsdavid By all means jump in David; thanks for sharing on.\nAnd if it stirs any thoughts ...",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hopkinsdavid",
+					"name": "David Hopkins",
+					"id": 19582633,
+					"id_str": "19582633",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1107351290852634600,
+		"in_reply_to_status_id_str": "1107351290852634624",
+		"in_reply_to_user_id": 19582633,
+		"in_reply_to_user_id_str": "19582633",
+		"in_reply_to_screen_name": "hopkinsdavid",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3229,
+			"friends_count": 3803,
+			"listed_count": 205,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 605,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11344,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@hopkinsdavid By all means jump in David; thanks for sharing on.\nAnd if it stirs any thoughts ..."
+	},
+	{
 		"created_at": "Sat Mar 16 18:44:58 +0000 2019",
 		"id": 1106904214645760000,
 		"id_str": "1106989751389876226",
