21dba7c32f9ef918e73142fc7f5e36393735b756
IaninSheffield <newreg@guesty.me.uk>
2019-05-13 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11447,
-	"created_at": "Mon May 06 01:15:57 +0000 2019",
+	"tweets": 11448,
+	"created_at": "Mon May 13 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 12 16:10:54 +0000 2019",
+		"id": 1126954458448113700,
+		"id_str": "1127607088597934087",
+		"full_text": "I cannot believe someone is selling the resource that I spent hours making on TES for £5! @TesResources what are you going to do about this... seriously? 😡 Can as many people as possible report please! Here is the link!  https://t.co/xwp6A0cFfq https://t.co/lnbEgdUdW8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TesResources",
+					"name": "Tes Resources",
+					"id": 33856511,
+					"id_str": "33856511",
+					"indices": [
+						90,
+						103
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/xwp6A0cFfq",
+					"expanded_url": "https://www.tes.com/teaching-resource/ks2-y6-sats-reading-comprehension-practice-question-styles-and-technique-12117091",
+					"display_url": "tes.com/teaching-resou…",
+					"indices": [
+						221,
+						244
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1126954450587934700,
+					"id_str": "1126954450587934722",
+					"indices": [
+						245,
+						268
+					],
+					"media_url": "http://pbs.twimg.com/media/D6O_GhlW4AIfIVf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6O_GhlW4AIfIVf.jpg",
+					"url": "https://t.co/lnbEgdUdW8",
+					"display_url": "pic.twitter.com/lnbEgdUdW8",
+					"expanded_url": "https://twitter.com/Miss__Wheatley/status/1126954458448113664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1126954450587934700,
+					"id_str": "1126954450587934722",
+					"indices": [
+						245,
+						268
+					],
+					"media_url": "http://pbs.twimg.com/media/D6O_GhlW4AIfIVf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6O_GhlW4AIfIVf.jpg",
+					"url": "https://t.co/lnbEgdUdW8",
+					"display_url": "pic.twitter.com/lnbEgdUdW8",
+					"expanded_url": "https://twitter.com/Miss__Wheatley/status/1126954458448113664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 846438053950476300,
+			"id_str": "846438053950476288",
+			"name": "★ Miss Wheatley ★ 🙋‍♀️",
+			"screen_name": "Miss__Wheatley",
+			"location": "Blackpool, England",
+			"description": "KS2 Lead Teacher. Currently posted in Y6. Showbie Champion🏆 Passionate about innovative and inspiring teaching. Views are all my own 😊",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1971,
+			"friends_count": 1426,
+			"listed_count": 12,
+			"created_at": "Mon Mar 27 19:05:32 +0000 2017",
+			"favourites_count": 4237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2339,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1090509747130957824/L3V8G3kc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090509747130957824/L3V8G3kc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/846438053950476288/1551047171",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 37,
+		"favorite_count": 63,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I cannot believe someone is selling the resource that I spent hours making on TES for £5! @TesResources what are you going to do about this... seriously? 😡 Can as many people as possible report please! Here is the link!  https://t.co/xwp6A0cFfq https://t.co/lnbEgdUdW8"
+	},
+	{
 		"created_at": "Sun May 05 20:30:35 +0000 2019",
 		"id": 1125135725270380500,
 		"id_str": "1125135725270380546",
