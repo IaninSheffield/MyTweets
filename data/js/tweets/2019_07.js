f5cc089289c07963b12484edc1bc7f273aea9a33
IaninSheffield <newreg@guesty.me.uk>
2019-07-03 02:15:58 +0100

    Added 1 tweets

---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,139 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Tue Jul 02 18:51:32 +0000 2019",
+		"id": 1146129293975740400,
+		"id_str": "1146129293975740416",
+		"full_text": "@Bookbird14 @Editage @AcademicChatter @sheffhallamuni Wait, what?! I've still not got to grips with imposter syndrome and now you're giving me another?! 😭",
+		"truncated": false,
+		"display_text_range": [
+			54,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Bookbird14",
+					"name": "Bookbird64",
+					"id": 1141302848048644100,
+					"id_str": "1141302848048644096",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "Editage",
+					"name": "Editage Insights",
+					"id": 78873087,
+					"id_str": "78873087",
+					"indices": [
+						12,
+						20
+					]
+				},
+				{
+					"screen_name": "AcademicChatter",
+					"name": "Academic Chatter",
+					"id": 885953601067208700,
+					"id_str": "885953601067208705",
+					"indices": [
+						21,
+						37
+					]
+				},
+				{
+					"screen_name": "sheffhallamuni",
+					"name": "Sheffield Hallam University",
+					"id": 20593971,
+					"id_str": "20593971",
+					"indices": [
+						38,
+						53
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1145948687845093400,
+		"in_reply_to_status_id_str": "1145948687845093376",
+		"in_reply_to_user_id": 1141302848048644100,
+		"in_reply_to_user_id_str": "1141302848048644096",
+		"in_reply_to_screen_name": "Bookbird14",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3247,
+			"friends_count": 3819,
+			"listed_count": 203,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 611,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Bookbird14 @Editage @AcademicChatter @sheffhallamuni Wait, what?! I've still not got to grips with imposter syndrome and now you're giving me another?! 😭"
+	}
+]
