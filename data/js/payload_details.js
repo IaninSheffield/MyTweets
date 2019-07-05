cba056760e91178964d08bddd607d32c5c0978fa
IaninSheffield <newreg@guesty.me.uk>
2019-07-05 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11482,
-	"created_at": "Thu Jul 04 01:15:57 +0000 2019",
+	"tweets": 11483,
+	"created_at": "Fri Jul 05 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,273 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 18:41:57 +0000 2019",
+		"id": 1146845074329919500,
+		"id_str": "1146851659936649222",
+		"full_text": "Learn to analyse Twitter data for academic research at this 1 day workshop at @sheffhallamuni\n\nSocial Media &amp; Digital Humanities: Social Network Analysis Using \n@NodeXL\n\nhttps://t.co/Yua8QkAgeK\n\n#PhDChat #SocialMedia #Dataviz #DigitalSociety #AcademicTwitter https://t.co/Rn4QS7sJRU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PhDChat",
+					"indices": [
+						199,
+						207
+					]
+				},
+				{
+					"text": "SocialMedia",
+					"indices": [
+						208,
+						220
+					]
+				},
+				{
+					"text": "Dataviz",
+					"indices": [
+						221,
+						229
+					]
+				},
+				{
+					"text": "DigitalSociety",
+					"indices": [
+						230,
+						245
+					]
+				},
+				{
+					"text": "AcademicTwitter",
+					"indices": [
+						246,
+						262
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sheffhallamuni",
+					"name": "Sheffield Hallam University",
+					"id": 20593971,
+					"id_str": "20593971",
+					"indices": [
+						78,
+						93
+					]
+				},
+				{
+					"screen_name": "nodexl",
+					"name": "NodeXL Project",
+					"id": 87606674,
+					"id_str": "87606674",
+					"indices": [
+						165,
+						172
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Yua8QkAgeK",
+					"expanded_url": "https://www.eventbrite.com/e/social-media-digital-humanities-social-network-analysis-using-nodexl-tickets-59532362900",
+					"display_url": "eventbrite.com/e/social-media…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1146845053802950700,
+					"id_str": "1146845053802950656",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1146845053802950656/pu/img/6z4LNJcCROaRGXdA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1146845053802950656/pu/img/6z4LNJcCROaRGXdA.jpg",
+					"url": "https://t.co/Rn4QS7sJRU",
+					"display_url": "pic.twitter.com/Rn4QS7sJRU",
+					"expanded_url": "https://twitter.com/SonSocMed/status/1146845074329919490/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146845053802950700,
+					"id_str": "1146845053802950656",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1146845053802950656/pu/img/6z4LNJcCROaRGXdA.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1146845053802950656/pu/img/6z4LNJcCROaRGXdA.jpg",
+					"url": "https://t.co/Rn4QS7sJRU",
+					"display_url": "pic.twitter.com/Rn4QS7sJRU",
+					"expanded_url": "https://twitter.com/SonSocMed/status/1146845074329919490/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 512,
+							"h": 512,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 6000,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1146845053802950656/pu/vid/512x512/GCQr0NSzaKe869Xa.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1146845053802950656/pu/pl/7ArFhk0PCEv5ddWw.m3u8?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1146845053802950656/pu/vid/320x320/4B6YVdEz48vN7joi.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 761985198967914500,
+			"id_str": "761985198967914496",
+			"name": "Social Media Workshop",
+			"screen_name": "SonSocMed",
+			"location": "UK ",
+			"description": "Social Media & Digital Humanities: Social Network Analysis Using @NodeXL. Taking place at Sheffield Hallam University Aug 16, 2019. Workshop Instructor @was3210",
+			"url": "https://t.co/itdMjNzeqn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itdMjNzeqn",
+							"expanded_url": "https://www.eventbrite.com/e/social-media-digital-humanities-social-network-analysis-using-nodexl-ti",
+							"display_url": "eventbrite.com/e/social-media…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 673,
+			"friends_count": 699,
+			"listed_count": 30,
+			"created_at": "Sat Aug 06 18:00:03 +0000 2016",
+			"favourites_count": 1194,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 973,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1132335395528691712/161rXVij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1132335395528691712/161rXVij_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/761985198967914496/1558804352",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Learn to analyse Twitter data for academic research at this 1 day workshop at @sheffhallamuni\n\nSocial Media &amp; Digital Humanities: Social Network Analysis Using \n@NodeXL\n\nhttps://t.co/Yua8QkAgeK\n\n#PhDChat #SocialMedia #Dataviz #DigitalSociety #AcademicTwitter https://t.co/Rn4QS7sJRU"
+	},
+	{
 		"created_at": "Wed Jul 03 06:58:18 +0000 2019",
 		"id": 1146312192154886100,
 		"id_str": "1146312192154886144",
