d583a552990fe29c13099960c35165cce1082815
IaninSheffield <newreg@guesty.me.uk>
2019-08-17 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11488,
-	"created_at": "Fri Aug 02 01:15:57 +0000 2019",
+	"tweets": 11489,
+	"created_at": "Sat Aug 17 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 16 17:27:43 +0000 2019",
+		"id": 1162415655561416700,
+		"id_str": "1162415655561416706",
+		"full_text": "Learned about the 'Science by Bike' project @sciencebybike  whilst catching up episode 6 of the @CyclingEurope podcast. Brilliant idea!\nIf I'd known I could have contributed to @ProjectSplatter whilst on my recent tour. Next time.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sciencebybike",
+					"name": "Science by Bike",
+					"id": 977508240060756000,
+					"id_str": "977508240060755968",
+					"indices": [
+						44,
+						58
+					]
+				},
+				{
+					"screen_name": "CyclingEurope",
+					"name": "Andrew P. Sykes",
+					"id": 29018406,
+					"id_str": "29018406",
+					"indices": [
+						96,
+						110
+					]
+				},
+				{
+					"screen_name": "ProjectSplatter",
+					"name": "Project Splatter",
+					"id": 1098970009,
+					"id_str": "1098970009",
+					"indices": [
+						177,
+						193
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3244,
+			"friends_count": 3815,
+			"listed_count": 200,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 619,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11432,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Learned about the 'Science by Bike' project @sciencebybike  whilst catching up episode 6 of the @CyclingEurope podcast. Brilliant idea!\nIf I'd known I could have contributed to @ProjectSplatter whilst on my recent tour. Next time."
+	},
+	{
 		"created_at": "Thu Aug 01 19:09:57 +0000 2019",
 		"id": 1157005565123604500,
 		"id_str": "1157005565123604480",
