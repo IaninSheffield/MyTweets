6ef409bb47c34f2610c9ccaf36858f4265178632
IaninSheffield <newreg@guesty.me.uk>
2019-08-28 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11490,
-	"created_at": "Fri Aug 23 01:15:57 +0000 2019",
+	"tweets": 11491,
+	"created_at": "Wed Aug 28 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 27 19:24:08 +0000 2019",
+		"id": 1166431216935035000,
+		"id_str": "1166431216935034887",
+		"full_text": "@dannynic @Alby @zephwright @russeltarr Yup. And much earlier than when I launched that tweet.",
+		"truncated": false,
+		"display_text_range": [
+			40,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dannynic",
+					"name": "Danny Nicholson",
+					"id": 10378262,
+					"id_str": "10378262",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "Alby",
+					"name": "Alby",
+					"id": 6795192,
+					"id_str": "6795192",
+					"indices": [
+						10,
+						15
+					]
+				},
+				{
+					"screen_name": "zephwright",
+					"name": "Joe",
+					"id": 376407024,
+					"id_str": "376407024",
+					"indices": [
+						16,
+						27
+					]
+				},
+				{
+					"screen_name": "russeltarr",
+					"name": "Russel Tarr",
+					"id": 16912160,
+					"id_str": "16912160",
+					"indices": [
+						28,
+						39
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1166097273648013300,
+		"in_reply_to_status_id_str": "1166097273648013313",
+		"in_reply_to_user_id": 10378262,
+		"in_reply_to_user_id_str": "10378262",
+		"in_reply_to_screen_name": "dannynic",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3242,
+			"friends_count": 3814,
+			"listed_count": 199,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 620,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11434,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@dannynic @Alby @zephwright @russeltarr Yup. And much earlier than when I launched that tweet."
+	},
+	{
 		"created_at": "Thu Aug 22 19:38:41 +0000 2019",
 		"id": 1164622939548987400,
 		"id_str": "1164622939548987393",
