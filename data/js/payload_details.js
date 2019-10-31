31a67e178875f522e9bb9fec2b87aa6a99c9fb25
IaninSheffield <newreg@guesty.me.uk>
2019-10-31 01:15:59 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11503,
-	"created_at": "Thu Oct 24 01:15:58 +0000 2019",
+	"tweets": 11504,
+	"created_at": "Thu Oct 31 01:15:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 30 20:46:52 +0000 2019",
+		"id": 1189644863047946200,
+		"id_str": "1189644863047946240",
+		"full_text": "I'm a sucker for #visualisations, especially interactive ones, but this just went straight into my top ten:\n\"Ye Olde Mad-Lib Pub Crawl Generator\" https://t.co/aGX5eFaKGO by @puddingviz \nEnter a pub name &amp; it maps the shortest route between them all.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "visualisations",
+					"indices": [
+						17,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "puddingviz",
+					"name": "The Pudding",
+					"id": 798297731961913300,
+					"id_str": "798297731961913344",
+					"indices": [
+						173,
+						184
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/aGX5eFaKGO",
+					"expanded_url": "https://pudding.cool/2019/10/pubs/",
+					"display_url": "pudding.cool/2019/10/pubs/",
+					"indices": [
+						146,
+						169
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3248,
+			"friends_count": 3809,
+			"listed_count": 196,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 632,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11449,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm a sucker for #visualisations, especially interactive ones, but this just went straight into my top ten:\n\"Ye Olde Mad-Lib Pub Crawl Generator\" https://t.co/aGX5eFaKGO by @puddingviz \nEnter a pub name &amp; it maps the shortest route between them all."
+	},
+	{
 		"created_at": "Wed Oct 23 21:22:23 +0000 2019",
 		"id": 1187117085220098000,
 		"id_str": "1187117085220098052",
