e9a88a3961b2aa685d71ebac84a67619e30519f5
IaninSheffield <newreg@guesty.me.uk>
2019-11-22 01:15:58 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11509,
-	"created_at": "Thu Nov 21 01:15:57 +0000 2019",
+	"tweets": 11510,
+	"created_at": "Fri Nov 22 01:15:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Thu Nov 21 20:03:08 +0000 2019",
+		"id": 1197606391495757800,
+		"id_str": "1197606391495757825",
+		"full_text": "'Gonna finish' - that's all that matters. https://t.co/Rhou1IT08j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Rhou1IT08j",
+					"expanded_url": "https://twitter.com/SHannamSwain/status/1197585358063575040",
+					"display_url": "twitter.com/SHannamSwain/s…",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3245,
+			"friends_count": 3808,
+			"listed_count": 196,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 635,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11455,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1197585358063575000,
+		"quoted_status_id_str": "1197585358063575040",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Rhou1IT08j",
+			"expanded": "https://twitter.com/SHannamSwain/status/1197585358063575040",
+			"display": "twitter.com/SHannamSwain/s…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 21 18:39:34 +0000 2019",
+			"id": 1197585358063575000,
+			"id_str": "1197585358063575040",
+			"full_text": "@Nick5307 @LauranDoak @drchrisjbailey @IaninSheffield @Rach_Handforth It was so good to see you all! I’m so glad I started my PhD when I did even if I’m gonna finish way afterwards!",
+			"truncated": false,
+			"display_text_range": [
+				70,
+				181
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Nick5307",
+						"name": "Nicholas Marshall",
+						"id": 2803369512,
+						"id_str": "2803369512",
+						"indices": [
+							0,
+							9
+						]
+					},
+					{
+						"screen_name": "LauranDoak",
+						"name": "Dr Lauran Doak",
+						"id": 1042495925871173600,
+						"id_str": "1042495925871173632",
+						"indices": [
+							10,
+							21
+						]
+					},
+					{
+						"screen_name": "drchrisjbailey",
+						"name": "drchrisjbailey",
+						"id": 1149595237343359000,
+						"id_str": "1149595237343358976",
+						"indices": [
+							22,
+							37
+						]
+					},
+					{
+						"screen_name": "IaninSheffield",
+						"name": "Ian Guest",
+						"id": 21324520,
+						"id_str": "21324520",
+						"indices": [
+							38,
+							53
+						]
+					},
+					{
+						"screen_name": "Rach_Handforth",
+						"name": "Dr Rachel Handforth",
+						"id": 119696660,
+						"id_str": "119696660",
+						"indices": [
+							54,
+							69
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1197581142867808300,
+			"in_reply_to_status_id_str": "1197581142867808256",
+			"in_reply_to_user_id": 2803369512,
+			"in_reply_to_user_id_str": "2803369512",
+			"in_reply_to_screen_name": "Nick5307",
+			"user": {
+				"id": 139021931,
+				"id_str": "139021931",
+				"name": "⚫️ Steph Hannam-Swain 👩🏼‍🦼",
+				"screen_name": "SHannamSwain",
+				"location": "England",
+				"description": "Disabled PhD student & Lecturer in Education at Sheffield Hallam University. Researching Disabled peoples’ conceptualisations&experiences of self-harm. She/her",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 809,
+				"friends_count": 569,
+				"listed_count": 10,
+				"created_at": "Sat May 01 09:19:00 +0000 2010",
+				"favourites_count": 18763,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4471,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1111933121178619906/ahazzK6D_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111933121178619906/ahazzK6D_normal.jpg",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'Gonna finish' - that's all that matters. https://t.co/Rhou1IT08j"
+	},
+	{
 		"created_at": "Wed Nov 20 21:15:31 +0000 2019",
 		"id": 1197262216896094200,
 		"id_str": "1197262216896094208",
