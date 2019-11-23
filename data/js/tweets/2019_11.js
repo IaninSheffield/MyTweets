76b294778a138ee3423cc3361b160174e65a5da5
IaninSheffield <newreg@guesty.me.uk>
2019-11-23 01:15:58 +0000

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 07:03:32 +0000 2019",
+		"id": 1197772585729175600,
+		"id_str": "1197772585729175552",
+		"full_text": "#BBCR4 Seriously podcast 'Hurting' https://t.co/fzqyVSGYQG possibly of interest to @SHannamSwain, but may be too general? Helpful to further raise awareness of the issue? Or not?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BBCR4",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SHannamSwain",
+					"name": "⚫️ Steph Hannam-Swain 👩🏼‍🦼",
+					"id": 139021931,
+					"id_str": "139021931",
+					"indices": [
+						83,
+						96
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/fzqyVSGYQG",
+					"expanded_url": "https://www.bbc.co.uk/programmes/p07vqgsd",
+					"display_url": "bbc.co.uk/programmes/p07…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3244,
+			"friends_count": 3808,
+			"listed_count": 196,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 640,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11457,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BBCR4 Seriously podcast 'Hurting' https://t.co/fzqyVSGYQG possibly of interest to @SHannamSwain, but may be too general? Helpful to further raise awareness of the issue? Or not?"
+	},
+	{
 		"created_at": "Thu Nov 21 20:03:08 +0000 2019",
 		"id": 1197606391495757800,
 		"id_str": "1197606391495757825",
@@ -183,7 +310,7 @@ Grailbird.data.tweets_2019_11 =
 			"user": {
 				"id": 139021931,
 				"id_str": "139021931",
-				"name": "⚫️ Steph Hannam-Swain 👩🏼‍🦼",
+				"name": "⚫️ Steph Hannam-Swain 👩🏼🦼",
 				"screen_name": "SHannamSwain",
 				"location": "England",
 				"description": "Disabled PhD student & Lecturer in Education at Sheffield Hallam University. Researching Disabled peoples’ conceptualisations&experiences of self-harm. She/her",
