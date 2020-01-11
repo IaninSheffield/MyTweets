cf8f59524e6fdfd43ec7eccac1fc3c095ad68af6
IaninSheffield <newreg@guesty.me.uk>
2020-01-11 01:16:01 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11515,
-	"created_at": "Sat Jan 04 01:15:57 +0000 2020",
+	"tweets": 11516,
+	"created_at": "Sat Jan 11 01:16:00 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,332 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 10 19:48:38 +0000 2020",
+		"id": 1215549480583540700,
+		"id_str": "1215722133151723521",
+		"full_text": "Great to see that \n#PosthumanistPerspectives \nis among this year's themes in the #IP2020report! \n\nhttps://t.co/5zTjvGTWoa \n\n@IaninSheffield \n@richardjholme \n@UrsulaStickler \n@Alison_Cambs https://t.co/Nlq9jnWVXh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PosthumanistPerspectives",
+					"indices": [
+						19,
+						44
+					]
+				},
+				{
+					"text": "IP2020report",
+					"indices": [
+						81,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IaninSheffield",
+					"name": "Ian Guest",
+					"id": 21324520,
+					"id_str": "21324520",
+					"indices": [
+						124,
+						139
+					]
+				},
+				{
+					"screen_name": "richardjholme",
+					"name": "richard holme",
+					"id": 209598541,
+					"id_str": "209598541",
+					"indices": [
+						141,
+						155
+					]
+				},
+				{
+					"screen_name": "UrsulaStickler",
+					"name": "Ursula Stickler",
+					"id": 153420382,
+					"id_str": "153420382",
+					"indices": [
+						157,
+						172
+					]
+				},
+				{
+					"screen_name": "Alison_Cambs",
+					"name": "Alison Fox",
+					"id": 465981903,
+					"id_str": "465981903",
+					"indices": [
+						174,
+						187
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5zTjvGTWoa",
+					"expanded_url": "http://www.open.ac.uk/blogs/innovating/?page_id=524",
+					"display_url": "open.ac.uk/blogs/innovati…",
+					"indices": [
+						98,
+						121
+					]
+				},
+				{
+					"url": "https://t.co/Nlq9jnWVXh",
+					"expanded_url": "https://twitter.com/agneskh/status/1215209308578816002",
+					"display_url": "twitter.com/agneskh/status…",
+					"indices": [
+						188,
+						211
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 843365802,
+			"id_str": "843365802",
+			"name": "Martina Emke",
+			"screen_name": "MartinaEmke",
+			"location": "",
+			"description": "Project Officer; Doctorate in Education @OpenUniversity ; Language Teacher Educator:  https://t.co/RAydICvtUZ ; Social Media Editor @JIME_journal",
+			"url": "https://t.co/EJQ282gkqD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EJQ282gkqD",
+							"expanded_url": "https://de.linkedin.com/in/martina-emke-2485565b",
+							"display_url": "de.linkedin.com/in/martina-emk…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/RAydICvtUZ",
+							"expanded_url": "http://www.ecml.at/ict-rev",
+							"display_url": "ecml.at/ict-rev",
+							"indices": [
+								86,
+								109
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1306,
+			"friends_count": 859,
+			"listed_count": 145,
+			"created_at": "Mon Sep 24 10:57:08 +0000 2012",
+			"favourites_count": 1821,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9813,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135614052149972993/r1sjjdvd_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135614052149972993/r1sjjdvd_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/843365802/1559588738",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1215209308578816000,
+		"quoted_status_id_str": "1215209308578816002",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Nlq9jnWVXh",
+			"expanded": "https://twitter.com/agneskh/status/1215209308578816002",
+			"display": "twitter.com/agneskh/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jan 09 09:50:51 +0000 2020",
+			"id": 1215209308578816000,
+			"id_str": "1215209308578816002",
+			"full_text": "Proud to announce the launch of our 2020 Innovating Pedagogy report - 10 new themes to explore! Enjoy! https://t.co/ih5VVsgCQb @OpenTEL_OU @OpenUniversity #IP2020report",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				168
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "IP2020report",
+						"indices": [
+							155,
+							168
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "OpenTEL_OU",
+						"name": "OpenTEL",
+						"id": 4525211661,
+						"id_str": "4525211661",
+						"indices": [
+							127,
+							138
+						]
+					},
+					{
+						"screen_name": "OpenUniversity",
+						"name": "The Open University",
+						"id": 14049061,
+						"id_str": "14049061",
+						"indices": [
+							139,
+							154
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/ih5VVsgCQb",
+						"expanded_url": "http://www.open.ac.uk/blogs/innovating/",
+						"display_url": "open.ac.uk/blogs/innovati…",
+						"indices": [
+							103,
+							126
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 12590832,
+				"id_str": "12590832",
+				"name": "Agnes Kukulska-Hulme",
+				"screen_name": "agneskh",
+				"location": "",
+				"description": "Professor at The Open University researching future learning, mobile language learning, migrant learning, intelligent assistants",
+				"url": "https://t.co/4h9jc3uG0f",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4h9jc3uG0f",
+								"expanded_url": "http://oro.open.ac.uk/view/person/ak35.html",
+								"display_url": "oro.open.ac.uk/view/person/ak…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2205,
+				"friends_count": 1219,
+				"listed_count": 141,
+				"created_at": "Wed Jan 23 12:11:20 +0000 2008",
+				"favourites_count": 1215,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1383,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EDECE9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1181518817433927682/ovQm7sJy_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181518817433927682/ovQm7sJy_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/12590832/1439819154",
+				"profile_link_color": "088253",
+				"profile_sidebar_border_color": "D3D2CF",
+				"profile_sidebar_fill_color": "E3E2DE",
+				"profile_text_color": "634047",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 29,
+			"favorite_count": 58,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great to see that \n#PosthumanistPerspectives \nis among this year's themes in the #IP2020report! \n\nhttps://t.co/5zTjvGTWoa \n\n@IaninSheffield \n@richardjholme \n@UrsulaStickler \n@Alison_Cambs https://t.co/Nlq9jnWVXh"
+	},
+	{
 		"created_at": "Fri Jan 03 21:29:50 +0000 2020",
 		"id": 1213210885633773600,
 		"id_str": "1213210885633773569",
