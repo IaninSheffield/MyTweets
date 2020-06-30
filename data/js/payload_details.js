af97d08d9455055ba27cc06e7aed7e18776ca41f
IaninSheffield <newreg@guesty.me.uk>
2020-06-30 02:15:59 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11548,
-	"created_at": "Mon Jun 22 01:15:57 +0000 2020",
+	"tweets": 11549,
+	"created_at": "Tue Jun 30 01:15:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 29 15:48:58 +0000 2020",
+		"id": 1277630146011422700,
+		"id_str": "1277630146011422720",
+		"full_text": "Indeed!\nEnjoyed meeting our @GROW_Mentoring Graduate Mentors. Welcoming, well-informed, articulate and thoughtful people. Looking forward to working with them. https://t.co/s844dtfY3F",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GROW_Mentoring",
+					"name": "GROW_Mentoring",
+					"id": 1275786754054914000,
+					"id_str": "1275786754054914048",
+					"indices": [
+						28,
+						43
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/s844dtfY3F",
+					"expanded_url": "https://twitter.com/dead_of_night/status/1277622168357085184",
+					"display_url": "twitter.com/dead_of_night/…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3200,
+			"friends_count": 3797,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 696,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11502,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1277622168357085200,
+		"quoted_status_id_str": "1277622168357085184",
+		"quoted_status_permalink": {
+			"url": "https://t.co/s844dtfY3F",
+			"expanded": "https://twitter.com/dead_of_night/status/1277622168357085184",
+			"display": "twitter.com/dead_of_night/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 29 15:17:16 +0000 2020",
+			"id": 1277622168357085200,
+			"id_str": "1277622168357085184",
+			"full_text": "Day 1 - Introductions @GROW_Mentoring lovely to meet our Graduate Mentors and to receive their '3 words to describe our first day' @SueOBrien19 @lizpollypocketp @IaninSheffield https://t.co/D17pgt0DYS",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				176
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "GROW_Mentoring",
+						"name": "GROW_Mentoring",
+						"id": 1275786754054914000,
+						"id_str": "1275786754054914048",
+						"indices": [
+							22,
+							37
+						]
+					},
+					{
+						"screen_name": "SueOBrien19",
+						"name": "Sue O'Brien",
+						"id": 1283185682,
+						"id_str": "1283185682",
+						"indices": [
+							131,
+							143
+						]
+					},
+					{
+						"screen_name": "lizpollypocketp",
+						"name": "Liz Perry",
+						"id": 4833395920,
+						"id_str": "4833395920",
+						"indices": [
+							144,
+							160
+						]
+					},
+					{
+						"screen_name": "IaninSheffield",
+						"name": "Ian Guest",
+						"id": 21324520,
+						"id_str": "21324520",
+						"indices": [
+							161,
+							176
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1277621826672382000,
+						"id_str": "1277621826672381953",
+						"indices": [
+							177,
+							200
+						],
+						"media_url": "http://pbs.twimg.com/media/EbsGSEAX0AEMNVf.png",
+						"media_url_https": "https://pbs.twimg.com/media/EbsGSEAX0AEMNVf.png",
+						"url": "https://t.co/D17pgt0DYS",
+						"display_url": "pic.twitter.com/D17pgt0DYS",
+						"expanded_url": "https://twitter.com/dead_of_night/status/1277622168357085184/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1277621826672382000,
+						"id_str": "1277621826672381953",
+						"indices": [
+							177,
+							200
+						],
+						"media_url": "http://pbs.twimg.com/media/EbsGSEAX0AEMNVf.png",
+						"media_url_https": "https://pbs.twimg.com/media/EbsGSEAX0AEMNVf.png",
+						"url": "https://t.co/D17pgt0DYS",
+						"display_url": "pic.twitter.com/D17pgt0DYS",
+						"expanded_url": "https://twitter.com/dead_of_night/status/1277622168357085184/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 21296925,
+				"id_str": "21296925",
+				"name": "Richard Pountney",
+				"screen_name": "dead_of_night",
+				"location": "Sheffield",
+				"description": "Researching curriculum, open education and design for learning. Interested in sociology of education, especially social realism and code theory.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 446,
+				"friends_count": 370,
+				"listed_count": 13,
+				"created_at": "Thu Feb 19 13:19:31 +0000 2009",
+				"favourites_count": 1130,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1935,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FF6699",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme11/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme11/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/105456193/treeclimber_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/105456193/treeclimber_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21296925/1415282176",
+				"profile_link_color": "B40B43",
+				"profile_sidebar_border_color": "CC3366",
+				"profile_sidebar_fill_color": "E5507E",
+				"profile_text_color": "362720",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Indeed!\nEnjoyed meeting our @GROW_Mentoring Graduate Mentors. Welcoming, well-informed, articulate and thoughtful people. Looking forward to working with them. https://t.co/s844dtfY3F"
+	},
+	{
 		"created_at": "Sun Jun 21 20:50:54 +0000 2020",
 		"id": 1274665991193190400,
 		"id_str": "1274807027089448960",
