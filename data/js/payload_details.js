5e5437fd8d22d8d4efcd48ab1a84cf2c510860d0
IaninSheffield <newreg@guesty.me.uk>
2020-07-02 02:15:59 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11549,
-	"created_at": "Tue Jun 30 01:15:58 +0000 2020",
+	"tweets": 11550,
+	"created_at": "Thu Jul 02 01:15:58 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,249 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 15:53:02 +0000 2020",
+		"id": 1278355945580617700,
+		"id_str": "1278355945580617729",
+		"full_text": "@Joga5 @dajbelshaw @Ideas_Factory @catherinelliott @MoreThanMaths @Vickycarl @dughall @DeputyMitchell @damoward @mister_jim @primarypete_ @TomSale @TheoKL I remember arriving fairly early. Few folks about. And though I'd never met them before, @Joga5 &amp; @dughall gave me a right royal ribbing, I think because of the motorbike gear.\nBut hey, they never needed an excuse to dole out the mockery ;-)",
+		"truncated": false,
+		"display_text_range": [
+			155,
+			400
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Joga5",
+					"name": "Bill Lord",
+					"id": 22745646,
+					"id_str": "22745646",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "dajbelshaw",
+					"name": "˗ˏˋ Doug Belshaw ˎˊ˗ 🇪🇺☠️✊",
+					"id": 764365,
+					"id_str": "764365",
+					"indices": [
+						7,
+						18
+					]
+				},
+				{
+					"screen_name": "Ideas_Factory",
+					"name": "Julian S Wood",
+					"id": 19065686,
+					"id_str": "19065686",
+					"indices": [
+						19,
+						33
+					]
+				},
+				{
+					"screen_name": "catherinelliott",
+					"name": "Catherine Elliott 🇪🇺",
+					"id": 19596048,
+					"id_str": "19596048",
+					"indices": [
+						34,
+						50
+					]
+				},
+				{
+					"screen_name": "MoreThanMaths",
+					"name": "Lois Lindemann",
+					"id": 14204276,
+					"id_str": "14204276",
+					"indices": [
+						51,
+						65
+					]
+				},
+				{
+					"screen_name": "Vickycarl",
+					"name": "Vicky Harrison",
+					"id": 21921300,
+					"id_str": "21921300",
+					"indices": [
+						66,
+						76
+					]
+				},
+				{
+					"screen_name": "dughall",
+					"name": "Dughall McCormick",
+					"id": 18716016,
+					"id_str": "18716016",
+					"indices": [
+						77,
+						85
+					]
+				},
+				{
+					"screen_name": "DeputyMitchell",
+					"name": "DeputyMitchell",
+					"id": 52426895,
+					"id_str": "52426895",
+					"indices": [
+						86,
+						101
+					]
+				},
+				{
+					"screen_name": "damoward",
+					"name": "damoward",
+					"id": 16981899,
+					"id_str": "16981899",
+					"indices": [
+						102,
+						111
+					]
+				},
+				{
+					"screen_name": "mister_jim",
+					"name": "Jim Maloney",
+					"id": 44078014,
+					"id_str": "44078014",
+					"indices": [
+						112,
+						123
+					]
+				},
+				{
+					"screen_name": "primarypete_",
+					"name": "Peter Richardson 🙋🏻‍♂️",
+					"id": 68169919,
+					"id_str": "68169919",
+					"indices": [
+						124,
+						137
+					]
+				},
+				{
+					"screen_name": "TomSale",
+					"name": "Thomas Sale MCCT",
+					"id": 19330969,
+					"id_str": "19330969",
+					"indices": [
+						138,
+						146
+					]
+				},
+				{
+					"screen_name": "TheoKL",
+					"name": "Theo Kuechel",
+					"id": 1290061,
+					"id_str": "1290061",
+					"indices": [
+						147,
+						154
+					]
+				},
+				{
+					"screen_name": "Joga5",
+					"name": "Bill Lord",
+					"id": 22745646,
+					"id_str": "22745646",
+					"indices": [
+						244,
+						250
+					]
+				},
+				{
+					"screen_name": "dughall",
+					"name": "Dughall McCormick",
+					"id": 18716016,
+					"id_str": "18716016",
+					"indices": [
+						257,
+						265
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1278332182415761400,
+		"in_reply_to_status_id_str": "1278332182415761420",
+		"in_reply_to_user_id": 22745646,
+		"in_reply_to_user_id_str": "22745646",
+		"in_reply_to_screen_name": "Joga5",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3200,
+			"friends_count": 3797,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 716,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11503,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Joga5 @dajbelshaw @Ideas_Factory @catherinelliott @MoreThanMaths @Vickycarl @dughall @DeputyMitchell @damoward @mister_jim @primarypete_ @TomSale @TheoKL I remember arriving fairly early. Few folks about. And though I'd never met them before, @Joga5 &amp; @dughall gave me a right royal ribbing, I think because of the motorbike gear.\nBut hey, they never needed an excuse to dole out the mockery ;-)"
+	}
+]
