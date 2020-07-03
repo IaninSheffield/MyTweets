96bdb815295d86175d55d145be478ca1f3f8dda4
IaninSheffield <newreg@guesty.me.uk>
2020-07-03 02:15:59 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11550,
-	"created_at": "Thu Jul 02 01:15:58 +0000 2020",
+	"tweets": 11551,
+	"created_at": "Fri Jul 03 01:15:57 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 02 19:49:38 +0000 2020",
+		"id": 1278777874028417000,
+		"id_str": "1278777874028417028",
+		"full_text": "@DeputyMitchell Have you tried turning it off and on again?\n\nIf that doesn't work restore from backup.\n\nYou *did* have a backup didn't you? https://t.co/MzPJfvqNrS",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			139
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DeputyMitchell",
+					"name": "DeputyMitchell",
+					"id": 52426895,
+					"id_str": "52426895",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1278777864314220500,
+					"id_str": "1278777864314220545",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Eb8hsT0U8AEg8iD.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Eb8hsT0U8AEg8iD.jpg",
+					"url": "https://t.co/MzPJfvqNrS",
+					"display_url": "pic.twitter.com/MzPJfvqNrS",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1278777874028417028/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278777864314220500,
+					"id_str": "1278777864314220545",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Eb8hsT0U8AEg8iD.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Eb8hsT0U8AEg8iD.jpg",
+					"url": "https://t.co/MzPJfvqNrS",
+					"display_url": "pic.twitter.com/MzPJfvqNrS",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1278777874028417028/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							139
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Eb8hsT0U8AEg8iD.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1278769145409089500,
+		"in_reply_to_status_id_str": "1278769145409089537",
+		"in_reply_to_user_id": 52426895,
+		"in_reply_to_user_id_str": "52426895",
+		"in_reply_to_screen_name": "DeputyMitchell",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3201,
+			"friends_count": 3797,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 716,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11504,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@DeputyMitchell Have you tried turning it off and on again?\n\nIf that doesn't work restore from backup.\n\nYou *did* have a backup didn't you? https://t.co/MzPJfvqNrS"
+	},
+	{
 		"created_at": "Wed Jul 01 15:53:02 +0000 2020",
 		"id": 1278355945580617700,
 		"id_str": "1278355945580617729",
@@ -116,7 +314,7 @@ Grailbird.data.tweets_2020_07 =
 				},
 				{
 					"screen_name": "primarypete_",
-					"name": "Peter Richardson 🙋🏻‍♂️",
+					"name": "Peter Richardson 🙋🏻♂️",
 					"id": 68169919,
 					"id_str": "68169919",
 					"indices": [
