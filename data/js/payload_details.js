56f390fd2df26b012ac1e83ea144bca3e8b00dd1
IaninSheffield <newreg@guesty.me.uk>
2020-09-11 02:15:58 +0100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11574,
-	"created_at": "Wed Sep 09 01:15:57 +0000 2020",
+	"tweets": 11578,
+	"created_at": "Fri Sep 11 01:15:57 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,612 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 10 20:38:48 +0000 2020",
+		"id": 1304157397498429400,
+		"id_str": "1304157397498429440",
+		"full_text": "Any #garmin gurus out there explain to me why my Touring Edge insists on booting to this screen if the ambient temp is &lt; about 20°C?\n\nOr better yet, how I can resolve it.\nUnfortunately #garmin support haven't helped. https://t.co/IFPS3dpQjy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "garmin",
+					"indices": [
+						4,
+						11
+					]
+				},
+				{
+					"text": "garmin",
+					"indices": [
+						188,
+						195
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1304156706306502700,
+					"id_str": "1304156706306502658",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/EhlLnVrXsAIG9XK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhlLnVrXsAIG9XK.jpg",
+					"url": "https://t.co/IFPS3dpQjy",
+					"display_url": "pic.twitter.com/IFPS3dpQjy",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1304157397498429440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1304156706306502700,
+					"id_str": "1304156706306502658",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/EhlLnVrXsAIG9XK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EhlLnVrXsAIG9XK.jpg",
+					"url": "https://t.co/IFPS3dpQjy",
+					"display_url": "pic.twitter.com/IFPS3dpQjy",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1304157397498429440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3180,
+			"friends_count": 3798,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Any #garmin gurus out there explain to me why my Touring Edge insists on booting to this screen if the ambient temp is &lt; about 20°C?\n\nOr better yet, how I can resolve it.\nUnfortunately #garmin support haven't helped. https://t.co/IFPS3dpQjy"
+	},
+	{
+		"created_at": "Thu Sep 10 20:13:24 +0000 2020",
+		"id": 1304151004414455800,
+		"id_str": "1304151004414455813",
+		"full_text": "@drchrisjbailey First came across naches in 'Reality is Broken: Why Games Make Us Better and How They Can Change the World' by @avantgame \n\nSometimes expressed as vicarious pride.",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			179
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drchrisjbailey",
+					"name": "Chris Bailey",
+					"id": 1149595237343359000,
+					"id_str": "1149595237343358976",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "avantgame",
+					"name": "Jane McGonigal",
+					"id": 681813,
+					"id_str": "681813",
+					"indices": [
+						127,
+						137
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1304150067163660300,
+		"in_reply_to_status_id_str": "1304150067163660302",
+		"in_reply_to_user_id": 1149595237343359000,
+		"in_reply_to_user_id_str": "1149595237343358976",
+		"in_reply_to_screen_name": "drchrisjbailey",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3180,
+			"friends_count": 3798,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@drchrisjbailey First came across naches in 'Reality is Broken: Why Games Make Us Better and How They Can Change the World' by @avantgame \n\nSometimes expressed as vicarious pride."
+	},
+	{
+		"created_at": "Thu Sep 10 20:08:56 +0000 2020",
+		"id": 1304149881586671600,
+		"id_str": "1304149881586671616",
+		"full_text": "@drchrisjbailey Neither of those 😆\n\nThis one - https://t.co/InJewcrviT https://t.co/XwLzGZLv0z",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drchrisjbailey",
+					"name": "Chris Bailey",
+					"id": 1149595237343359000,
+					"id_str": "1149595237343358976",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/InJewcrviT",
+					"expanded_url": "https://en.wiktionary.org/wiki/naches",
+					"display_url": "en.wiktionary.org/wiki/naches",
+					"indices": [
+						47,
+						70
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1304149585137406000,
+					"id_str": "1304149585137405952",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/EhlFI1QWkAAJkeX.png",
+					"media_url_https": "https://pbs.twimg.com/media/EhlFI1QWkAAJkeX.png",
+					"url": "https://t.co/XwLzGZLv0z",
+					"display_url": "pic.twitter.com/XwLzGZLv0z",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1304149881586671616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 108,
+							"h": 108,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1304149585137406000,
+					"id_str": "1304149585137405952",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/EhlFI1QWkAAJkeX.png",
+					"media_url_https": "https://pbs.twimg.com/media/EhlFI1QWkAAJkeX.png",
+					"url": "https://t.co/XwLzGZLv0z",
+					"display_url": "pic.twitter.com/XwLzGZLv0z",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1304149881586671616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 108,
+							"h": 108,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 463,
+							"h": 108,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1304145135513538600,
+		"in_reply_to_status_id_str": "1304145135513538561",
+		"in_reply_to_user_id": 1149595237343359000,
+		"in_reply_to_user_id_str": "1149595237343358976",
+		"in_reply_to_screen_name": "drchrisjbailey",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3180,
+			"friends_count": 3798,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@drchrisjbailey Neither of those 😆\n\nThis one - https://t.co/InJewcrviT https://t.co/XwLzGZLv0z"
+	},
+	{
+		"created_at": "Thu Sep 10 19:47:16 +0000 2020",
+		"id": 1304144430308491300,
+		"id_str": "1304144430308491264",
+		"full_text": "@drchrisjbailey But of course! Thank you.\nI think that's called 'nache' is it not?",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drchrisjbailey",
+					"name": "Chris Bailey",
+					"id": 1149595237343359000,
+					"id_str": "1149595237343358976",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1304139153739833300,
+		"in_reply_to_status_id_str": "1304139153739833345",
+		"in_reply_to_user_id": 1149595237343359000,
+		"in_reply_to_user_id_str": "1149595237343358976",
+		"in_reply_to_screen_name": "drchrisjbailey",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3180,
+			"friends_count": 3798,
+			"listed_count": 193,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@drchrisjbailey But of course! Thank you.\nI think that's called 'nache' is it not?"
+	},
+	{
 		"created_at": "Tue Sep 08 18:28:15 +0000 2020",
 		"id": 1303399767502917600,
 		"id_str": "1303399767502917633",
