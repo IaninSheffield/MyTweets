018f72276ba6331c3e59e4667af65cf14398a81d
IaninSheffield <newreg@guesty.me.uk>
2020-10-12 02:16:02 +0100

    Added 2 tweets

---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,293 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Sun Oct 11 20:42:51 +0000 2020",
+		"id": 1315392440807850000,
+		"id_str": "1315392440807849986",
+		"full_text": "DOING DATA DIFFERENTLY\nWe're launching the virtual exhibition from this research project between 16:30 &amp; 17:30 on Wed 11th Nov. online.\nOf interest to colleagues (esp. sr. leaders) interested in literacy in primary schools\nRegistration (free) https://t.co/Gjj8keRrVj - join us! https://t.co/inOm6mFT8H",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Gjj8keRrVj",
+					"expanded_url": "http://bit.ly/DDDExhibitionLaunch",
+					"display_url": "bit.ly/DDDExhibitionL…",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1315392430875848700,
+					"id_str": "1315392430875848707",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/EkE2claXsAMWWlY.png",
+					"media_url_https": "https://pbs.twimg.com/media/EkE2claXsAMWWlY.png",
+					"url": "https://t.co/inOm6mFT8H",
+					"display_url": "pic.twitter.com/inOm6mFT8H",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1315392440807849986/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 437,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315392430875848700,
+					"id_str": "1315392430875848707",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/EkE2claXsAMWWlY.png",
+					"media_url_https": "https://pbs.twimg.com/media/EkE2claXsAMWWlY.png",
+					"url": "https://t.co/inOm6mFT8H",
+					"display_url": "pic.twitter.com/inOm6mFT8H",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1315392440807849986/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 437,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3185,
+			"friends_count": 3802,
+			"listed_count": 195,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 742,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11546,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "DOING DATA DIFFERENTLY\nWe're launching the virtual exhibition from this research project between 16:30 &amp; 17:30 on Wed 11th Nov. online.\nOf interest to colleagues (esp. sr. leaders) interested in literacy in primary schools\nRegistration (free) https://t.co/Gjj8keRrVj - join us! https://t.co/inOm6mFT8H"
+	},
+	{
+		"created_at": "Sun Oct 11 20:26:14 +0000 2020",
+		"id": 1315388259959287800,
+		"id_str": "1315388259959287809",
+		"full_text": "@LauranDoak You will be welcome of course Dr D ... and thank you kindly for the RT 👍",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LauranDoak",
+					"name": "Dr Lauran Doak 🇪🇺",
+					"id": 1042495925871173600,
+					"id_str": "1042495925871173632",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1315386726899167200,
+		"in_reply_to_status_id_str": "1315386726899167232",
+		"in_reply_to_user_id": 1042495925871173600,
+		"in_reply_to_user_id_str": "1042495925871173632",
+		"in_reply_to_screen_name": "LauranDoak",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3185,
+			"friends_count": 3802,
+			"listed_count": 195,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 742,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11546,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LauranDoak You will be welcome of course Dr D ... and thank you kindly for the RT 👍"
+	}
+]
