74093ce2284278ea870e2c73b7ad735d258c86e6
IaninSheffield <newreg@guesty.me.uk>
2020-10-15 02:15:59 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11586,
-	"created_at": "Tue Oct 13 01:15:59 +0000 2020",
+	"tweets": 11587,
+	"created_at": "Thu Oct 15 01:15:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 14 20:08:40 +0000 2020",
+		"id": 1316471000863854600,
+		"id_str": "1316471000863854593",
+		"full_text": "@erbradfield @sheffhallamuni You're welcome 🙂\nGlad you're able to join us.👍",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			75
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erbradfield",
+					"name": "Dr Emily Bradfield",
+					"id": 468599022,
+					"id_str": "468599022",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "sheffhallamuni",
+					"name": "Sheffield Hallam University",
+					"id": 20593971,
+					"id_str": "20593971",
+					"indices": [
+						13,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1316469546878996500,
+		"in_reply_to_status_id_str": "1316469546878996481",
+		"in_reply_to_user_id": 468599022,
+		"in_reply_to_user_id_str": "468599022",
+		"in_reply_to_screen_name": "erbradfield",
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3185,
+			"friends_count": 3802,
+			"listed_count": 195,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 749,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@erbradfield @sheffhallamuni You're welcome 🙂\nGlad you're able to join us.👍"
+	},
+	{
 		"created_at": "Mon Oct 12 20:04:07 +0000 2020",
 		"id": 1315745081412980700,
 		"id_str": "1315745081412980738",
@@ -359,7 +475,7 @@ Grailbird.data.tweets_2020_10 =
 				"name": "Ian Eagleton #TheRealm",
 				"screen_name": "MrEagletonIan",
 				"location": "South East",
-				"description": "🏳️‍🌈 Part-time teacher, resource writer. Creator of The Reading Realm, an app for children @reading_realm 📚 The Meaning of Mariah Carey",
+				"description": "🏳️🌈 Part-time teacher, resource writer. Creator of The Reading Realm, an app for children @reading_realm 📚 The Meaning of Mariah Carey",
 				"url": "https://t.co/shsfjFDY9P",
 				"entities": {
 					"url": {
