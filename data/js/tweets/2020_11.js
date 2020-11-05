283d59ce32027ba64b1c84f29b2f3a626f6157eb
IaninSheffield <newreg@guesty.me.uk>
2020-11-05 01:15:59 +0000

    Added 1 tweets

--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 04 19:34:53 +0000 2020",
+		"id": 1324072646108217300,
+		"id_str": "1324072646108217344",
+		"full_text": "Final week before the online exhibition launch event on 11th Nov. Might be of interest to #Headteacher / #SLT in primary schools.\nLink to sign up in the tweet below https://t.co/8B7OFFcWUa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Headteacher",
+					"indices": [
+						90,
+						102
+					]
+				},
+				{
+					"text": "SLT",
+					"indices": [
+						105,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8B7OFFcWUa",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1315707038840819720",
+					"display_url": "twitter.com/IaninSheffield…",
+					"indices": [
+						165,
+						188
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3182,
+			"friends_count": 3801,
+			"listed_count": 195,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11568,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1315707038840819700,
+		"quoted_status_id_str": "1315707038840819720",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8B7OFFcWUa",
+			"expanded": "https://twitter.com/IaninSheffield/status/1315707038840819720",
+			"display": "twitter.com/IaninSheffield…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Oct 12 17:32:57 +0000 2020",
+			"id": 1315707038840819700,
+			"id_str": "1315707038840819720",
+			"full_text": "DOING DATA DIFFERENTLY\nWe're launching the virtual exhibition from this research project between 16:30 &amp; 17:30 on Wed 11th Nov. online.\nOf interest to colleagues (esp. sr. leaders) interested in literacy in primary schools\nRegistration (free) https://t.co/Gjj8keRrVj - join us! https://t.co/WzNnmIgQXo",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				281
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Gjj8keRrVj",
+						"expanded_url": "http://bit.ly/DDDExhibitionLaunch",
+						"display_url": "bit.ly/DDDExhibitionL…",
+						"indices": [
+							247,
+							270
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1315706941960839200,
+						"id_str": "1315706941960839169",
+						"indices": [
+							282,
+							305
+						],
+						"media_url": "http://pbs.twimg.com/media/EkJUfigXYAEjwHH.png",
+						"media_url_https": "https://pbs.twimg.com/media/EkJUfigXYAEjwHH.png",
+						"url": "https://t.co/WzNnmIgQXo",
+						"display_url": "pic.twitter.com/WzNnmIgQXo",
+						"expanded_url": "https://twitter.com/IaninSheffield/status/1315707038840819720/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 569,
+								"h": 886,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 569,
+								"h": 886,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 437,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1315706941960839200,
+						"id_str": "1315706941960839169",
+						"indices": [
+							282,
+							305
+						],
+						"media_url": "http://pbs.twimg.com/media/EkJUfigXYAEjwHH.png",
+						"media_url_https": "https://pbs.twimg.com/media/EkJUfigXYAEjwHH.png",
+						"url": "https://t.co/WzNnmIgQXo",
+						"display_url": "pic.twitter.com/WzNnmIgQXo",
+						"expanded_url": "https://twitter.com/IaninSheffield/status/1315707038840819720/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 569,
+								"h": 886,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 569,
+								"h": 886,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 437,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1315707034638131200,
+			"in_reply_to_status_id_str": "1315707034638131201",
+			"in_reply_to_user_id": 21324520,
+			"in_reply_to_user_id_str": "21324520",
+			"in_reply_to_screen_name": "IaninSheffield",
+			"user": {
+				"id": 21324520,
+				"id_str": "21324520",
+				"name": "Ian Guest",
+				"screen_name": "IaninSheffield",
+				"location": "Sheffield, Yorkshire",
+				"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+				"url": "https://t.co/SUuFhgyQxD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/SUuFhgyQxD",
+								"expanded_url": "https://cpdin140.wordpress.com",
+								"display_url": "cpdin140.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3182,
+				"friends_count": 3801,
+				"listed_count": 195,
+				"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+				"favourites_count": 754,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 11568,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DEDAF4",
+				"profile_text_color": "A05D0E",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Final week before the online exhibition launch event on 11th Nov. Might be of interest to #Headteacher / #SLT in primary schools.\nLink to sign up in the tweet below https://t.co/8B7OFFcWUa"
+	},
+	{
 		"created_at": "Tue Nov 03 19:39:02 +0000 2020",
 		"id": 1323711300405547000,
 		"id_str": "1323711300405547008",
