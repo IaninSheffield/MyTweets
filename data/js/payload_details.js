78c409fd211d826dc60d8c5c43ad18f34a1c1daf
IaninSheffield <newreg@guesty.me.uk>
2020-11-12 01:15:59 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11607,
-	"created_at": "Wed Nov 11 01:15:58 +0000 2020",
+	"tweets": 11608,
+	"created_at": "Thu Nov 12 01:15:58 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 11 15:59:11 +0000 2020",
+		"id": 1326555080108114000,
+		"id_str": "1326555080108113923",
+		"full_text": "Only half an hour to go to the 'DOING DATA DIFFERENTLY Virtual Exhibition Launch event.\nLooking forward to welcoming attendees from far and wide. https://t.co/qw3nlx5IQu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1326555054296412200,
+					"id_str": "1326555054296412161",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EmjeyyJWMAEIkx7.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmjeyyJWMAEIkx7.png",
+					"url": "https://t.co/qw3nlx5IQu",
+					"display_url": "pic.twitter.com/qw3nlx5IQu",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1326555080108113923/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 437,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1326555054296412200,
+					"id_str": "1326555054296412161",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EmjeyyJWMAEIkx7.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmjeyyJWMAEIkx7.png",
+					"url": "https://t.co/qw3nlx5IQu",
+					"display_url": "pic.twitter.com/qw3nlx5IQu",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1326555080108113923/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 569,
+							"h": 886,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 437,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3184,
+			"friends_count": 3806,
+			"listed_count": 195,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 762,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11575,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Only half an hour to go to the 'DOING DATA DIFFERENTLY Virtual Exhibition Launch event.\nLooking forward to welcoming attendees from far and wide. https://t.co/qw3nlx5IQu"
+	},
+	{
 		"created_at": "Tue Nov 10 15:13:35 +0000 2020",
 		"id": 1326181213967691800,
 		"id_str": "1326181213967691776",
@@ -76,7 +250,7 @@ Grailbird.data.tweets_2020_11 =
 				},
 				{
 					"screen_name": "SHannamSwain",
-					"name": "Steph Hannam-Swain 👩🏼‍🦼",
+					"name": "Steph Hannam-Swain 👩🏼🦼",
 					"id": 139021931,
 					"id_str": "139021931",
 					"indices": [
