c17be8b9701f87cd28339c679041561da3964380
IaninSheffield <newreg@guesty.me.uk>
2021-05-15 02:15:57 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11655,
-	"created_at": "Fri May 14 01:15:57 +0000 2021",
+	"tweets": 11656,
+	"created_at": "Sat May 15 01:15:57 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 14 11:10:02 +0000 2021",
+		"id": 1393161677441482800,
+		"id_str": "1393161677441482758",
+		"full_text": "Collated set of resources, several of which I found particularly useful when thesis writing. #phdchat https://t.co/Qo4bjdrV3W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "phdchat",
+					"indices": [
+						93,
+						101
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Qo4bjdrV3W",
+					"expanded_url": "https://twitter.com/DocwritingSIG/status/1393087424255954946",
+					"display_url": "twitter.com/DocwritingSIG/…",
+					"indices": [
+						102,
+						125
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3161,
+			"friends_count": 3780,
+			"listed_count": 190,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 770,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11632,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1393087424255955000,
+		"quoted_status_id_str": "1393087424255954946",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Qo4bjdrV3W",
+			"expanded": "https://twitter.com/DocwritingSIG/status/1393087424255954946",
+			"display": "twitter.com/DocwritingSIG/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 14 06:14:58 +0000 2021",
+			"id": 1393087424255955000,
+			"id_str": "1393087424255954946",
+			"full_text": "Online resources for doctoral writers: an annotated bibliography https://t.co/9nEtoZrFcY",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				88
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/9nEtoZrFcY",
+						"expanded_url": "https://doctoralwriting.wordpress.com/2021/05/14/online-resources-for-doctoral-writers-an-annotated-bibliography/",
+						"display_url": "doctoralwriting.wordpress.com/2021/05/14/onl…",
+						"indices": [
+							65,
+							88
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 583506897,
+				"id_str": "583506897",
+				"name": "Doctoralwriting",
+				"screen_name": "DocwritingSIG",
+				"location": "Australia",
+				"description": "Twitter account of the Doctoral Writing Special Interest Group http://t.co/diz01DvHgE",
+				"url": "http://t.co/rvDwGWGMA7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/rvDwGWGMA7",
+								"expanded_url": "http://doctoralwriting.wordpress.com/",
+								"display_url": "doctoralwriting.wordpress.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "http://t.co/diz01DvHgE",
+								"expanded_url": "http://doctoralwriting.wordpress.com/about/",
+								"display_url": "doctoralwriting.wordpress.com/about/",
+								"indices": [
+									63,
+									85
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 15308,
+				"friends_count": 28,
+				"listed_count": 203,
+				"created_at": "Fri May 18 03:34:21 +0000 2012",
+				"favourites_count": 119,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 631,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2228042695/paper_piles_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2228042695/paper_piles_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Collated set of resources, several of which I found particularly useful when thesis writing. #phdchat https://t.co/Qo4bjdrV3W"
+	},
+	{
 		"created_at": "Thu May 13 20:45:22 +0000 2021",
 		"id": 1392944077214306300,
 		"id_str": "1392944077214306313",
