396dfde8d41abfefa8035153e9b84ef0827730bb
IaninSheffield <newreg@guesty.me.uk>
2021-10-01 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11663,
-	"created_at": "Thu Sep 16 01:15:57 +0000 2021",
+	"tweets": 11664,
+	"created_at": "Fri Oct 01 01:15:57 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 30 17:37:40 +0000 2021",
+		"id": 1443610567059689500,
+		"id_str": "1443631142247321604",
+		"full_text": "Joss Lowden breaks the UCI women's hour record, 48.406 km https://t.co/5E1AngY9Hg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			57
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1443610508741972000,
+					"id_str": "1443610508741971969",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/FAi8G--WEAE8-ji.png",
+					"media_url_https": "https://pbs.twimg.com/media/FAi8G--WEAE8-ji.png",
+					"url": "https://t.co/5E1AngY9Hg",
+					"display_url": "pic.twitter.com/5E1AngY9Hg",
+					"expanded_url": "https://twitter.com/inrng/status/1443610567059689480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 456,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 413,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 456,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1443610508741972000,
+					"id_str": "1443610508741971969",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/FAi8G--WEAE8-ji.png",
+					"media_url_https": "https://pbs.twimg.com/media/FAi8G--WEAE8-ji.png",
+					"url": "https://t.co/5E1AngY9Hg",
+					"display_url": "pic.twitter.com/5E1AngY9Hg",
+					"expanded_url": "https://twitter.com/inrng/status/1443610567059689480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 456,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 413,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 456,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 118685634,
+			"id_str": "118685634",
+			"name": "the Inner Ring",
+			"screen_name": "inrng",
+			"location": "Eurozone",
+			"description": "News, chatter and interaction from The Inner Ring cycling blog.",
+			"url": "https://t.co/VfJiOv3fXT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VfJiOv3fXT",
+							"expanded_url": "https://www.inrng.com",
+							"display_url": "inrng.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 81113,
+			"friends_count": 1289,
+			"listed_count": 2517,
+			"created_at": "Mon Mar 01 13:09:25 +0000 2010",
+			"favourites_count": 11,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69794,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "D4D1E3",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2119908384/inrng_logo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2119908384/inrng_logo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/118685634/1621880096",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "C5CDF0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 78,
+		"favorite_count": 1256,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Joss Lowden breaks the UCI women's hour record, 48.406 km https://t.co/5E1AngY9Hg"
+	},
+	{
 		"created_at": "Wed Sep 15 07:18:45 +0000 2021",
 		"id": 1438039567597244400,
 		"id_str": "1438039567597244421",
