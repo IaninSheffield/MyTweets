8031395b372216fd0ec1505e1c5789454e5fbe73
IaninSheffield <newreg@guesty.me.uk>
2021-10-17 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11664,
-	"created_at": "Fri Oct 01 01:15:57 +0000 2021",
+	"tweets": 11665,
+	"created_at": "Sun Oct 17 01:15:57 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_10.js
@@ -0,0 +1,193 @@
+Grailbird.data.tweets_2021_10 = 
+[
+	{
+		"created_at": "Sat Oct 16 19:50:58 +0000 2021",
+		"id": 1449462893305553000,
+		"id_str": "1449462893305552902",
+		"full_text": "Went for a scoot around the 'Biography of a Fly' posters today - #otsbuzzz from Sheffield's 'Off the Shelf Festival' #OTSfest\n(If you give it a try, poster 8 is shifted forward to Broad St West, Arundel Gate underpass being closed atm) https://t.co/yirHJrF9B5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "otsbuzzz",
+					"indices": [
+						65,
+						74
+					]
+				},
+				{
+					"text": "OTSfest",
+					"indices": [
+						117,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1449462822027604000,
+					"id_str": "1449462822027603970",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/FB2Gwc4XMAI2YOg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB2Gwc4XMAI2YOg.jpg",
+					"url": "https://t.co/yirHJrF9B5",
+					"display_url": "pic.twitter.com/yirHJrF9B5",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1449462893305552902/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1449462822027604000,
+					"id_str": "1449462822027603970",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/FB2Gwc4XMAI2YOg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB2Gwc4XMAI2YOg.jpg",
+					"url": "https://t.co/yirHJrF9B5",
+					"display_url": "pic.twitter.com/yirHJrF9B5",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1449462893305552902/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3133,
+			"friends_count": 3759,
+			"listed_count": 189,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 772,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11638,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Went for a scoot around the 'Biography of a Fly' posters today - #otsbuzzz from Sheffield's 'Off the Shelf Festival' #OTSfest\n(If you give it a try, poster 8 is shifted forward to Broad St West, Arundel Gate underpass being closed atm) https://t.co/yirHJrF9B5"
+	}
+]
