5972419624db699fb3d3c7c71098f86944528a25
IaninSheffield <newreg@guesty.me.uk>
2021-11-05 01:15:59 +0000

    Added 1 tweets

---
+++ data/js/tweets/2021_11.js
@@ -0,0 +1,265 @@
+Grailbird.data.tweets_2021_11 = 
+[
+	{
+		"created_at": "Thu Nov 04 20:54:04 +0000 2021",
+		"id": 1456364145708765200,
+		"id_str": "1456364145708765190",
+		"full_text": "Finally made a long overdue visit to the National Emergency Services Museum in #Sheffield today. Enjoyable &amp; informative slice of the social history of the city and wider world. #DiscoveredNESM\n\nSooo wanted a go on the sliding pole though! https://t.co/BIopEbDSU3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			243
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Sheffield",
+					"indices": [
+						79,
+						89
+					]
+				},
+				{
+					"text": "DiscoveredNESM",
+					"indices": [
+						182,
+						197
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1456363876086321200,
+					"id_str": "1456363876086321158",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/FDYLOqnWEAYwD8G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDYLOqnWEAYwD8G.jpg",
+					"url": "https://t.co/BIopEbDSU3",
+					"display_url": "pic.twitter.com/BIopEbDSU3",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1456364145708765190/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1078,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1456363876086321200,
+					"id_str": "1456363876086321158",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/FDYLOqnWEAYwD8G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDYLOqnWEAYwD8G.jpg",
+					"url": "https://t.co/BIopEbDSU3",
+					"display_url": "pic.twitter.com/BIopEbDSU3",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1456364145708765190/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1078,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456364003979104300,
+					"id_str": "1456364003979104257",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/FDYLWHDXEAEmQ5f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDYLWHDXEAEmQ5f.jpg",
+					"url": "https://t.co/BIopEbDSU3",
+					"display_url": "pic.twitter.com/BIopEbDSU3",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1456364145708765190/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1078,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456364082072854500,
+					"id_str": "1456364082072854539",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/FDYLap-XIAs5APa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDYLap-XIAs5APa.jpg",
+					"url": "https://t.co/BIopEbDSU3",
+					"display_url": "pic.twitter.com/BIopEbDSU3",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1456364145708765190/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1078,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3128,
+			"friends_count": 3759,
+			"listed_count": 187,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 773,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11642,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Finally made a long overdue visit to the National Emergency Services Museum in #Sheffield today. Enjoyable &amp; informative slice of the social history of the city and wider world. #DiscoveredNESM\n\nSooo wanted a go on the sliding pole though! https://t.co/BIopEbDSU3"
+	}
+]
