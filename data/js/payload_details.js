edcf698bd381090da6f8c715d5e79f5b94cdad10
IaninSheffield <newreg@guesty.me.uk>
2022-01-26 01:15:57 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11671,
-	"created_at": "Tue Jan 18 01:15:57 +0000 2022",
+	"tweets": 11672,
+	"created_at": "Wed Jan 26 01:15:57 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 21:35:09 +0000 2022",
+		"id": 1486090288879882200,
+		"id_str": "1486090288879882245",
+		"full_text": "\"Making Room for Zoom in Focus Group Methods: Opportunities and Challenges for Novice Researchers (During and Beyond COVID-19)\"\nWonder about the +ve &amp; -ve impacts of both familiarity with the platform, &amp; 'Zoom overload', as working practices have shifted.\n#phdchat https://t.co/ztyy6nLdF6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "phdchat",
+					"indices": [
+						264,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1486089481023275000,
+					"id_str": "1486089481023275012",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ-mgmTWQAQiAoP.png",
+					"media_url_https": "https://pbs.twimg.com/media/FJ-mgmTWQAQiAoP.png",
+					"url": "https://t.co/ztyy6nLdF6",
+					"display_url": "pic.twitter.com/ztyy6nLdF6",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1486090288879882245/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 823,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 823,
+							"h": 241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1486089481023275000,
+					"id_str": "1486089481023275012",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ-mgmTWQAQiAoP.png",
+					"media_url_https": "https://pbs.twimg.com/media/FJ-mgmTWQAQiAoP.png",
+					"url": "https://t.co/ztyy6nLdF6",
+					"display_url": "pic.twitter.com/ztyy6nLdF6",
+					"expanded_url": "https://twitter.com/IaninSheffield/status/1486090288879882245/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 823,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 823,
+							"h": 241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3127,
+			"friends_count": 3755,
+			"listed_count": 184,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 775,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11645,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Making Room for Zoom in Focus Group Methods: Opportunities and Challenges for Novice Researchers (During and Beyond COVID-19)\"\nWonder about the +ve &amp; -ve impacts of both familiarity with the platform, &amp; 'Zoom overload', as working practices have shifted.\n#phdchat https://t.co/ztyy6nLdF6"
+	},
+	{
 		"created_at": "Mon Jan 17 19:46:05 +0000 2022",
 		"id": 1483163735376150500,
 		"id_str": "1483163735376150533",
