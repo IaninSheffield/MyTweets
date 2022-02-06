4d021f0f0da5916ef965933e44dea3e77851faf1
IaninSheffield <newreg@guesty.me.uk>
2022-02-06 01:15:58 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11672,
-	"created_at": "Wed Jan 26 01:15:57 +0000 2022",
+	"tweets": 11673,
+	"created_at": "Sun Feb 06 01:15:57 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,110 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Sat Feb 05 10:36:03 +0000 2022",
+		"id": 1489901502428819500,
+		"id_str": "1489910686130098179",
+		"full_text": "Have you heard about the Cycle Touring Festival? Nine days of online talks and films, dedicated to the exploration of the world by bike. 12-20 Feb 2022. And it's free! Check it out here: https://t.co/S9RcI76tEg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/S9RcI76tEg",
+					"expanded_url": "https://cycletouringfestival.co.uk",
+					"display_url": "cycletouringfestival.co.uk",
+					"indices": [
+						187,
+						210
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2355071252,
+			"id_str": "2355071252",
+			"name": "CycleTouringFestival",
+			"screen_name": "CycleTourFest",
+			"location": "",
+			"description": "Follow for updates and visit our website to sign up to the newsletter.",
+			"url": "https://t.co/uvbi2lbxKC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uvbi2lbxKC",
+							"expanded_url": "http://www.cycletouringfestival.co.uk",
+							"display_url": "cycletouringfestival.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2809,
+			"friends_count": 937,
+			"listed_count": 58,
+			"created_at": "Fri Feb 21 16:54:32 +0000 2014",
+			"favourites_count": 1632,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2590,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/739434324810997760/gISGKygg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/739434324810997760/gISGKygg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2355071252/1409885035",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Have you heard about the Cycle Touring Festival? Nine days of online talks and films, dedicated to the exploration of the world by bike. 12-20 Feb 2022. And it's free! Check it out here: https://t.co/S9RcI76tEg"
+	}
+]
