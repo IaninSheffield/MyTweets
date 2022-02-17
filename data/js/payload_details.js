fda113aa7ecf01c9fffe56b92bfab57de5382f51
IaninSheffield <newreg@guesty.me.uk>
2022-02-17 01:15:57 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11673,
-	"created_at": "Sun Feb 06 01:15:57 +0000 2022",
+	"tweets": 11674,
+	"created_at": "Thu Feb 17 01:15:57 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 16 21:08:19 +0000 2022",
+		"id": 1494056068917469200,
+		"id_str": "1494056068917469189",
+		"full_text": "And a brilliant talk it was. https://t.co/Svy7iLL284 is such a useful, powerful, thoughtfully assembled, user friendly, cyle touring route planning resource.👍 https://t.co/YNtKfKHd0s",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Svy7iLL284",
+					"expanded_url": "https://cycle.travel/",
+					"display_url": "cycle.travel",
+					"indices": [
+						29,
+						52
+					]
+				},
+				{
+					"url": "https://t.co/YNtKfKHd0s",
+					"expanded_url": "https://twitter.com/CycleTourFest/status/1493943560315547650",
+					"display_url": "twitter.com/CycleTourFest/…",
+					"indices": [
+						159,
+						182
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3124,
+			"friends_count": 3755,
+			"listed_count": 0,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 778,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11647,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1493943560315547600,
+		"quoted_status_id_str": "1493943560315547650",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YNtKfKHd0s",
+			"expanded": "https://twitter.com/CycleTourFest/status/1493943560315547650",
+			"display": "twitter.com/CycleTourFest/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Feb 16 13:41:15 +0000 2022",
+			"id": 1493943560315547600,
+			"id_str": "1493943560315547650",
+			"full_text": "Route planning is the topic of tonight's talk. Join Richard Fairhurst at 8pm to hear about the bike journeys that led him to set up the popular cycle touring website, https://t.co/yh4TBqzmFs. https://t.co/r7qSJfHDV8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				215
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/yh4TBqzmFs",
+						"expanded_url": "http://cycle.travel",
+						"display_url": "cycle.travel",
+						"indices": [
+							167,
+							190
+						]
+					},
+					{
+						"url": "https://t.co/r7qSJfHDV8",
+						"expanded_url": "https://cycletouringfestival.co.uk/whatson/",
+						"display_url": "cycletouringfestival.co.uk/whatson/",
+						"indices": [
+							192,
+							215
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2355071252,
+				"id_str": "2355071252",
+				"name": "CycleTouringFestival",
+				"screen_name": "CycleTourFest",
+				"location": "",
+				"description": "Follow for updates and visit our website to sign up to the newsletter.",
+				"url": "https://t.co/uvbi2lbxKC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uvbi2lbxKC",
+								"expanded_url": "http://www.cycletouringfestival.co.uk",
+								"display_url": "cycletouringfestival.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2845,
+				"friends_count": 941,
+				"listed_count": 0,
+				"created_at": "Fri Feb 21 16:54:32 +0000 2014",
+				"favourites_count": 1659,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2623,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/739434324810997760/gISGKygg_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/739434324810997760/gISGKygg_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2355071252/1409885035",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "And a brilliant talk it was. https://t.co/Svy7iLL284 is such a useful, powerful, thoughtfully assembled, user friendly, cyle touring route planning resource.👍 https://t.co/YNtKfKHd0s"
+	},
+	{
 		"created_at": "Sat Feb 05 10:36:03 +0000 2022",
 		"id": 1489901502428819500,
 		"id_str": "1489910686130098179",
