2291a3379590304ed534c4620aa0bf7ce7aec9eb
IaninSheffield <newreg@guesty.me.uk>
2022-05-07 02:15:58 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11676,
-	"created_at": "Thu Mar 03 01:15:57 +0000 2022",
+	"tweets": 11677,
+	"created_at": "Sat May 07 01:15:57 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,322 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Fri May 06 18:24:52 +0000 2022",
+		"id": 1522643574428520400,
+		"id_str": "1522643574428520450",
+		"full_text": "Delighted to have been involved in this ongoing research project.\nLooking forward to hearing future findings. https://t.co/U6SNwyIZ6l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U6SNwyIZ6l",
+					"expanded_url": "https://twitter.com/remple_project/status/1522578618416779266",
+					"display_url": "twitter.com/remple_project…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3115,
+			"friends_count": 3747,
+			"listed_count": 183,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 774,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1522578618416779300,
+		"quoted_status_id_str": "1522578618416779266",
+		"quoted_status_permalink": {
+			"url": "https://t.co/U6SNwyIZ6l",
+			"expanded": "https://twitter.com/remple_project/status/1522578618416779266",
+			"display": "twitter.com/remple_project…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 06 14:06:45 +0000 2022",
+			"id": 1522578618416779300,
+			"id_str": "1522578618416779266",
+			"full_text": "Our article \"How does research reach teachers? An agenda for investigating research mobilities in primary literacy education\" is published today Open Access in Literacy @The_UKLA  @ESRC https://t.co/vl4fmVmhkE https://t.co/752BCCOySX",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				209
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "The_UKLA",
+						"name": "UKLA",
+						"id": 135507736,
+						"id_str": "135507736",
+						"indices": [
+							169,
+							178
+						]
+					},
+					{
+						"screen_name": "ESRC",
+						"name": "Economic and Social Research Council",
+						"id": 19393236,
+						"id_str": "19393236",
+						"indices": [
+							180,
+							185
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/vl4fmVmhkE",
+						"expanded_url": "http://doi.org/10.1111/lit.12289",
+						"display_url": "doi.org/10.1111/lit.12…",
+						"indices": [
+							186,
+							209
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1522578506080829400,
+						"id_str": "1522578506080829440",
+						"indices": [
+							210,
+							233
+						],
+						"media_url": "http://pbs.twimg.com/media/FSFJFZWXwAAv7Yd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FSFJFZWXwAAv7Yd.jpg",
+						"url": "https://t.co/752BCCOySX",
+						"display_url": "pic.twitter.com/752BCCOySX",
+						"expanded_url": "https://twitter.com/remple_project/status/1522578618416779266/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 581,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1633,
+								"h": 791,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 329,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1522578506080829400,
+						"id_str": "1522578506080829440",
+						"indices": [
+							210,
+							233
+						],
+						"media_url": "http://pbs.twimg.com/media/FSFJFZWXwAAv7Yd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FSFJFZWXwAAv7Yd.jpg",
+						"url": "https://t.co/752BCCOySX",
+						"display_url": "pic.twitter.com/752BCCOySX",
+						"expanded_url": "https://twitter.com/remple_project/status/1522578618416779266/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 581,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1633,
+								"h": 791,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 329,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1504426153553145900,
+				"id_str": "1504426153553145856",
+				"name": "Research Mobilities in Primary Literacy Education",
+				"screen_name": "remple_project",
+				"location": "",
+				"description": "Inquiring how literacy research evidence moves to, from & among primary teachers & what happens to it as it does so. Team:@sheffhallamuni @StirUni @LancasterUni",
+				"url": "https://t.co/wG8tvUXiwM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wG8tvUXiwM",
+								"expanded_url": "https://research.shu.ac.uk/rmple/",
+								"display_url": "research.shu.ac.uk/rmple/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 18,
+				"friends_count": 97,
+				"listed_count": 0,
+				"created_at": "Thu Mar 17 11:55:37 +0000 2022",
+				"favourites_count": 35,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 34,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1504426153553145856/1647600248",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Delighted to have been involved in this ongoing research project.\nLooking forward to hearing future findings. https://t.co/U6SNwyIZ6l"
+	}
+]
