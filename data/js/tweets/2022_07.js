85d2aaa8bdbc2f18f79e1a879f10c6da4b30bb6c
IaninSheffield <newreg@guesty.me.uk>
2022-07-21 02:15:58 +0100

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Wed Jul 20 18:53:14 +0000 2022",
+		"id": 1549829803037139000,
+		"id_str": "1549829803037138944",
+		"full_text": "Just catching up with my Future Tense podcasts (Does Data Science Need a Hippocratic Oath, https://t.co/FBTo9YXw1M) and look whose tweet not only gets a mention but prompts an episode! https://t.co/rNQRjxwpkf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			184
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FBTo9YXw1M",
+					"expanded_url": "https://www.abc.net.au/radionational/programs/futuretense/does-data-science-need-a-hippocratic-oath/13864888",
+					"display_url": "abc.net.au/radionational/…",
+					"indices": [
+						91,
+						114
+					]
+				},
+				{
+					"url": "https://t.co/rNQRjxwpkf",
+					"expanded_url": "https://twitter.com/mrkrndvs/status/1508399145404764167",
+					"display_url": "twitter.com/mrkrndvs/statu…",
+					"indices": [
+						185,
+						208
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21324520,
+			"id_str": "21324520",
+			"name": "Ian Guest",
+			"screen_name": "IaninSheffield",
+			"location": "Sheffield, Yorkshire",
+			"description": "Former Physics teacher, former Head of ELearning ... and still learning. PhD in teacher professional learning with Twitter.",
+			"url": "https://t.co/SUuFhgyQxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SUuFhgyQxD",
+							"expanded_url": "https://cpdin140.wordpress.com",
+							"display_url": "cpdin140.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3110,
+			"friends_count": 3748,
+			"listed_count": 183,
+			"created_at": "Thu Feb 19 18:28:04 +0000 2009",
+			"favourites_count": 779,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11662,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/476823938568163328/boGjK4q1_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21324520/1439662711",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DEDAF4",
+			"profile_text_color": "A05D0E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1508399145404764200,
+		"quoted_status_id_str": "1508399145404764167",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rNQRjxwpkf",
+			"expanded": "https://twitter.com/mrkrndvs/status/1508399145404764167",
+			"display": "twitter.com/mrkrndvs/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Mar 28 11:02:35 +0000 2022",
+			"id": 1508399145404764200,
+			"id_str": "1508399145404764167",
+			"full_text": "@antonyjfunnell enjoyed the discussion of forgetting https://t.co/08BOnjmhNR\n\nIt had me thinking about forgetting associated w/ technology. When it comes to big data, the focus is often on remembering everything. I wonder what is the place for forgetting in this situation?",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				273
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "antonyjfunnell",
+						"name": "Antony J Funnell",
+						"id": 25003408,
+						"id_str": "25003408",
+						"indices": [
+							0,
+							15
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/08BOnjmhNR",
+						"expanded_url": "https://www.abc.net.au/radionational/programs/futuretense/forgetting-not-memory-moves-us-forward/13798670",
+						"display_url": "abc.net.au/radionational/…",
+						"indices": [
+							53,
+							76
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 25003408,
+			"in_reply_to_user_id_str": "25003408",
+			"in_reply_to_screen_name": "antonyjfunnell",
+			"user": {
+				"id": 372768752,
+				"id_str": "372768752",
+				"name": "Aaron Davis 🏘️",
+				"screen_name": "mrkrndvs",
+				"location": "Melbourne, Australia",
+				"description": "#EdTech Coach. Interested in how together we can work to make a better world. #3Strengths: passion, curiosity & persistence. https://t.co/uTlsCeAwJ7",
+				"url": "https://t.co/9kvcoZA3IC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/9kvcoZA3IC",
+								"expanded_url": "https://collect.readwriterespond.com",
+								"display_url": "collect.readwriterespond.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/uTlsCeAwJ7",
+								"expanded_url": "https://readwriterespond.com",
+								"display_url": "readwriterespond.com",
+								"indices": [
+									125,
+									148
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 4066,
+				"friends_count": 726,
+				"listed_count": 351,
+				"created_at": "Tue Sep 13 11:36:49 +0000 2011",
+				"favourites_count": 4795,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 28395,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/708552669636333569/eIfl-Utk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/372768752/1462853542",
+				"profile_link_color": "91D2FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just catching up with my Future Tense podcasts (Does Data Science Need a Hippocratic Oath, https://t.co/FBTo9YXw1M) and look whose tweet not only gets a mention but prompts an episode! https://t.co/rNQRjxwpkf"
+	},
+	{
 		"created_at": "Tue Jul 12 18:04:27 +0000 2022",
 		"id": 1546918423397113900,
 		"id_str": "1546918423397113861",
