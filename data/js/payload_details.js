c896365bc02aafe1f7d4f84795d5e88af8779cd4
IaninSheffield <newreg@guesty.me.uk>
2022-10-04 02:16:02 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11698,
-	"created_at": "Mon Sep 26 01:16:01 +0000 2022",
+	"tweets": 11699,
+	"created_at": "Tue Oct 04 01:16:01 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,131 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Mon Oct 03 20:32:11 +0000 2022",
+		"id": 1576938591518740500,
+		"id_str": "1577033795555164160",
+		"full_text": "October already! A huge thank you to all who have already registered interest in the project – we’ll be in touch with dates for Focus Groups very soon. \nThere is still time to sign up – find out more here: https://t.co/PC03SkOB01\n@SHUCDARE  @The_UKLA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			250
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SHUCDARE",
+					"name": "SIRKE",
+					"id": 333540991,
+					"id_str": "333540991",
+					"indices": [
+						230,
+						239
+					]
+				},
+				{
+					"screen_name": "The_UKLA",
+					"name": "UKLA",
+					"id": 135507736,
+					"id_str": "135507736",
+					"indices": [
+						241,
+						250
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PC03SkOB01",
+					"expanded_url": "https://research.shu.ac.uk/rmple/get-involved/",
+					"display_url": "research.shu.ac.uk/rmple/get-invo…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1504426153553145900,
+			"id_str": "1504426153553145856",
+			"name": "Research Mobilities in Primary Literacy Education",
+			"screen_name": "remple_project",
+			"location": "UK",
+			"description": "Exploring how literacy research evidence moves to & among primary teachers & what happens to it as it does so. Team:@sheffhallamuni @StirUni @LancasterUni",
+			"url": "https://t.co/wG8tvUXiwM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wG8tvUXiwM",
+							"expanded_url": "https://research.shu.ac.uk/rmple/",
+							"display_url": "research.shu.ac.uk/rmple/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97,
+			"friends_count": 233,
+			"listed_count": 0,
+			"created_at": "Thu Mar 17 11:55:37 +0000 2022",
+			"favourites_count": 142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1504426153553145856/1647600248",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "October already! A huge thank you to all who have already registered interest in the project – we’ll be in touch with dates for Focus Groups very soon. \nThere is still time to sign up – find out more here: https://t.co/PC03SkOB01\n@SHUCDARE  @The_UKLA"
+	}
+]
