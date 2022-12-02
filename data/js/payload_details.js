b5a9251df982bcc06510029b1c9368f3968ffce6
IaninSheffield <newreg@guesty.me.uk>
2022-12-02 01:16:04 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11701,
-	"created_at": "Mon Nov 28 01:15:57 +0000 2022",
+	"tweets": 11702,
+	"created_at": "Fri Dec 02 01:16:02 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Thu Dec 01 19:24:44 +0000 2022",
+		"id": 1598181923989618700,
+		"id_str": "1598397706036281357",
+		"full_text": "Spacetime Soccer https://t.co/L58OAkCRCh https://t.co/C3IsGLemBJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/L58OAkCRCh",
+					"expanded_url": "http://xkcd.com/2705",
+					"display_url": "xkcd.com/2705",
+					"indices": [
+						17,
+						40
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1598181917177905200,
+					"id_str": "1598181917177905153",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi3h_HPXgAEBiaZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi3h_HPXgAEBiaZ.jpg",
+					"url": "https://t.co/C3IsGLemBJ",
+					"display_url": "pic.twitter.com/C3IsGLemBJ",
+					"expanded_url": "https://twitter.com/xkcd/status/1598181923989618688/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1480,
+							"h": 559,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 453,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 257,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598181917177905200,
+					"id_str": "1598181917177905153",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi3h_HPXgAEBiaZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi3h_HPXgAEBiaZ.jpg",
+					"url": "https://t.co/C3IsGLemBJ",
+					"display_url": "pic.twitter.com/C3IsGLemBJ",
+					"expanded_url": "https://twitter.com/xkcd/status/1598181923989618688/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1480,
+							"h": 559,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 453,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 257,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21146468,
+			"id_str": "21146468",
+			"name": "Randall Munroe",
+			"screen_name": "xkcd",
+			"location": "",
+			"description": "I draw the comic xkcd",
+			"url": "https://t.co/gADSbeGBoi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gADSbeGBoi",
+							"expanded_url": "http://xkcd.com",
+							"display_url": "xkcd.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 291395,
+			"friends_count": 1,
+			"listed_count": 2045,
+			"created_at": "Tue Feb 17 23:36:03 +0000 2009",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 795,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1488600831377252354/hEpPeSu0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1488600831377252354/hEpPeSu0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21146468/1663076236",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 467,
+		"favorite_count": 4417,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Spacetime Soccer https://t.co/L58OAkCRCh https://t.co/C3IsGLemBJ"
+	}
+]
