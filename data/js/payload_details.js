0a96df8aa17ebd96a324168bd0c6d1602d8cb400
IaninSheffield <newreg@guesty.me.uk>
2023-02-14 01:15:58 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11703,
-	"created_at": "Mon Jan 02 01:15:57 +0000 2023",
+	"tweets": 11704,
+	"created_at": "Tue Feb 14 01:15:57 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_02.js
@@ -0,0 +1,301 @@
+Grailbird.data.tweets_2023_02 = 
+[
+	{
+		"created_at": "Mon Feb 13 20:15:20 +0000 2023",
+		"id": 1624018855751286800,
+		"id_str": "1625227143419404323",
+		"full_text": "Are you a #primaryPE subject leader in England?  Join in with #research to investigate the role &amp; make your voice heard! 📣📣\n\n@ArchesSSP @LinksSSP @kiwiinsheffield @learnsheffield @DrGarethJones @SHU_AWRC @ThePEUmbrella @ActiveWCC @PPEA_org @LondonSport @YorkshireSport @afPE_PE https://t.co/BUSvfnPdMX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "primaryPE",
+					"indices": [
+						10,
+						20
+					]
+				},
+				{
+					"text": "research",
+					"indices": [
+						62,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ArchesSSP",
+					"name": "The Arches SSP",
+					"id": 532476460,
+					"id_str": "532476460",
+					"indices": [
+						129,
+						139
+					]
+				},
+				{
+					"screen_name": "LinksSSP",
+					"name": "Links SSP",
+					"id": 700705352,
+					"id_str": "700705352",
+					"indices": [
+						140,
+						149
+					]
+				},
+				{
+					"screen_name": "kiwiinsheffield",
+					"name": "Sarah Williams",
+					"id": 384990539,
+					"id_str": "384990539",
+					"indices": [
+						150,
+						166
+					]
+				},
+				{
+					"screen_name": "learnsheffield",
+					"name": "Learn Sheffield",
+					"id": 3586023196,
+					"id_str": "3586023196",
+					"indices": [
+						167,
+						182
+					]
+				},
+				{
+					"screen_name": "DrGarethJones",
+					"name": "Gareth Jones",
+					"id": 510193574,
+					"id_str": "510193574",
+					"indices": [
+						183,
+						197
+					]
+				},
+				{
+					"screen_name": "SHU_AWRC",
+					"name": "Advanced Wellbeing Research Centre",
+					"id": 1137435640235208700,
+					"id_str": "1137435640235208709",
+					"indices": [
+						198,
+						207
+					]
+				},
+				{
+					"screen_name": "ThePEUmbrella",
+					"name": "ℝ𝕪𝕒𝕟 𝔼𝕝𝕝𝕚𝕤",
+					"id": 1684268095,
+					"id_str": "1684268095",
+					"indices": [
+						208,
+						222
+					]
+				},
+				{
+					"screen_name": "ActiveWCC",
+					"name": "ActiveWestminster",
+					"id": 700792021,
+					"id_str": "700792021",
+					"indices": [
+						223,
+						233
+					]
+				},
+				{
+					"screen_name": "PPEA_org",
+					"name": "Primary Physical Education Assembly",
+					"id": 1235621253245014000,
+					"id_str": "1235621253245014018",
+					"indices": [
+						234,
+						243
+					]
+				},
+				{
+					"screen_name": "LondonSport",
+					"name": "London Sport",
+					"id": 46637283,
+					"id_str": "46637283",
+					"indices": [
+						244,
+						256
+					]
+				},
+				{
+					"screen_name": "YorkshireSport",
+					"name": "Yorkshire Sport Fndn",
+					"id": 28306037,
+					"id_str": "28306037",
+					"indices": [
+						257,
+						272
+					]
+				},
+				{
+					"screen_name": "afPE_PE",
+					"name": "Association for Physical Education",
+					"id": 190589939,
+					"id_str": "190589939",
+					"indices": [
+						273,
+						281
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1624008811257143300,
+					"id_str": "1624008811257143298",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/FomjaNMWIAIyoVJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FomjaNMWIAIyoVJ.jpg",
+					"url": "https://t.co/BUSvfnPdMX",
+					"display_url": "pic.twitter.com/BUSvfnPdMX",
+					"expanded_url": "https://twitter.com/kimlongbon/status/1624018855751286786/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1307,
+							"h": 732,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 672,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1624008811257143300,
+					"id_str": "1624008811257143298",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/FomjaNMWIAIyoVJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FomjaNMWIAIyoVJ.jpg",
+					"url": "https://t.co/BUSvfnPdMX",
+					"display_url": "pic.twitter.com/BUSvfnPdMX",
+					"expanded_url": "https://twitter.com/kimlongbon/status/1624018855751286786/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1307,
+							"h": 732,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 672,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 479013652,
+			"id_str": "479013652",
+			"name": "Kim Longbon",
+			"screen_name": "kimlongbon",
+			"location": "",
+			"description": "Senior Lecturer at Sheff Hallam Uni. Sport development. PhD Student. Triathlete. Dancer. GoodGymer. Trustee of @CroftTheatreCo. Beagles. Bereaved Sibling",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 419,
+			"friends_count": 614,
+			"listed_count": 8,
+			"created_at": "Mon Jan 30 22:35:19 +0000 2012",
+			"favourites_count": 3362,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1226,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/687580038884102144/-GuX_aGL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/687580038884102144/-GuX_aGL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/479013652/1489248039",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 27,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Are you a #primaryPE subject leader in England?  Join in with #research to investigate the role &amp; make your voice heard! 📣📣\n\n@ArchesSSP @LinksSSP @kiwiinsheffield @learnsheffield @DrGarethJones @SHU_AWRC @ThePEUmbrella @ActiveWCC @PPEA_org @LondonSport @YorkshireSport @afPE_PE https://t.co/BUSvfnPdMX"
+	}
+]
