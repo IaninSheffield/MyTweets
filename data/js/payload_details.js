03fe50ee288d56cf8e11a28f0aa4203fbb4c62b5
IaninSheffield <newreg@guesty.me.uk>
2023-05-18 02:15:57 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11707,
-	"created_at": "Fri Apr 28 01:15:58 +0000 2023",
+	"tweets": 11708,
+	"created_at": "Thu May 18 01:15:57 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Wed May 17 06:32:12 +0000 2023",
+		"id": 1651643325454098400,
+		"id_str": "1658722065258610690",
+		"full_text": "We have a new podcast! \n\nThis episode introduces what corpus linguistics is &amp; how we're using the method in our project. Dr Anna Lindroos Cermakova explains what analysing newspapers &amp; Twitter conversations tells us about how people understand &amp; talk about literacy in England. https://t.co/ZCUJFuFTYB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			289
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1651643309394206700,
+					"id_str": "1651643309394206722",
+					"indices": [
+						290,
+						313
+					],
+					"media_url": "http://pbs.twimg.com/media/FuvQ1tAXoAIxIE9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FuvQ1tAXoAIxIE9.jpg",
+					"url": "https://t.co/ZCUJFuFTYB",
+					"display_url": "pic.twitter.com/ZCUJFuFTYB",
+					"expanded_url": "https://twitter.com/remple_project/status/1651643325454098453/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1651643309394206700,
+					"id_str": "1651643309394206722",
+					"indices": [
+						290,
+						313
+					],
+					"media_url": "http://pbs.twimg.com/media/FuvQ1tAXoAIxIE9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FuvQ1tAXoAIxIE9.jpg",
+					"url": "https://t.co/ZCUJFuFTYB",
+					"display_url": "pic.twitter.com/ZCUJFuFTYB",
+					"expanded_url": "https://twitter.com/remple_project/status/1651643325454098453/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1504426153553145900,
+			"id_str": "1504426153553145856",
+			"name": "Research Mobilities in Primary Literacy Education",
+			"screen_name": "remple_project",
+			"location": "UK",
+			"description": "Exploring how literacy research evidence moves to & among primary teachers & what happens to it as it does so. Team:@sheffhallamuni @StirUni @LancasterUni",
+			"url": "https://t.co/wG8tvUXiwM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wG8tvUXiwM",
+							"expanded_url": "https://research.shu.ac.uk/rmple/",
+							"display_url": "research.shu.ac.uk/rmple/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 186,
+			"friends_count": 412,
+			"listed_count": 0,
+			"created_at": "Thu Mar 17 11:55:37 +0000 2022",
+			"favourites_count": 332,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 255,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1504770967297536004/z2QwC0Tv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1504426153553145856/1647600248",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We have a new podcast! \n\nThis episode introduces what corpus linguistics is &amp; how we're using the method in our project. Dr Anna Lindroos Cermakova explains what analysing newspapers &amp; Twitter conversations tells us about how people understand &amp; talk about literacy in England. https://t.co/ZCUJFuFTYB"
+	}
+]
